38bb09f97e46b1eda86a3f59a72cc002eacdc62e
lorka1 <lorena126os@gmail.com>
2023-09-08 14:48:12 +0200

    chore: improvement #39

--- packages/lib/src/stepper/stepper.wc.svelte
+++ packages/lib/src/stepper/stepper.wc.svelte
@@ -7,60 +7,41 @@
 
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
-
-  export let steps: string[] = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
-  export let labels: string[] = ['label 1', 'label 2', 'label 3', 'label 4'];
+  export let steps: string[] = ['Make an order', 'label 2', 'label 3', 'label 4', 'label 5'];
   export let step: number = 0;
-  export let finishingLabel: string = "You're finished.";
-
-  let finished = false;
-  let circleColors: string[] = [];
-
-
-  $: {
-    circleColors = Array.from({ length: steps.length }, (_, i) =>
-      i <= step ? 'circle-red' : 'circle-grey'
-    );
-  }
 
   export function next() {
     if (step < steps.length - 1) {
       step += 1;
     }
+    dispatch('next', {step})
   }
 
   export function previous() {
     if (step > 0) {
       step -= 1;
     }
-  }
-
-  export function finish() {
-    finished = true;
+    dispatch('previous', {step}) 
   }
 
   export function reset() {
     step = 0;
-    finished = false;
   }
 
   const dispatch = createEventDispatcher();
 
-  $: {
-    dispatch('previous', { step });
-    dispatch('next', { step });
-    
-  }
 </script>
 
 <div class="whole-div">
   <div class="step-class">
-    {#each steps as Step, index}
+    {#each steps as label, index}
       <div class="step-container">
-        <div class="circle {circleColors[index]}">
+        <div class="circle {index <= step ? 'circle-red' : 'circle-grey'}">
           <span class="number">{index + 1}</span>
         </div>
-        <p>{labels[index]}</p>
+        <div class="label">
+          {label}
+        </div>
       </div>
       {#if index < steps.length - 1}
           <div class="connector-line"></div> 
@@ -68,38 +49,7 @@
     {/each}
   </div>
 
-  {#if step > 0 && finished == true}
-    <div class="step">
-      <h1>{finishingLabel}</h1>
-    </div>
-  {:else}
-    <div class="step">
-      {steps[step]}
-    </div>
-  {/if}
-
-  <div class="button-row">
-    <div class="button-container">
-      {#if step > 0 && finished == false}
-        <button on:click={previous}>Back</button>
-      {/if}
-    </div>
-    <div class="button-container">
-      {#if step === 0}
-        <button on:click={next}>Next</button>
-      {:else if step === steps.length - 1}
-        {#if finished}
-          <button on:click={reset}>Reset</button>
-        {:else}
-          <button on:click={finish}>Finish</button>
-        {/if}
-      {:else}
-        <button on:click={next}>Next</button>
-      {/if}
-    </div>
   </div>
-</div>
-
 <style>
   .whole-div {
     width: 50%;
@@ -108,13 +58,14 @@
 
   .step-class {
     display: flex;
-    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
     gap: 16px;
     justify-content: space-between;
   }
-
+  
   .step-container {
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -122,11 +73,11 @@
     justify-content: space-between;
   } 
 
-  .step {
-    margin-top: 10px;
-    margin-left: 3px;
+  .label {
+    position: absolute;
+    margin-top: 103%;
+    line-height: 1.2;
   }
-
 
   .circle {
     width: 30px;
@@ -154,45 +105,11 @@
     font-weight: 600;
     color: white;
   }  
-    
-  .button-row {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 16px;
-  }
-
-  .button-container {
-    display: flex;
-    align-items: center;
-  }
-
-  button {
-    color: rgb(199, 64, 15);
-    padding: 3px 9px;
-    margin-bottom: 10px;
-    border-radius: 10px;
-  }
-
-  button:hover {
-    background-color: rgb(250, 245, 243);
-  }
-
-  button:active {
-    background: linear-gradient(
-      to right,
-      rgb(255, 228, 213),
-      rgb(255, 215, 200),
-      rgb(255, 237, 229)
-    );
-  }
 
   .connector-line {
-  width: 30%;
-  height: 1px; 
-  margin-bottom: 5%;
-  background-color: #ccc;
+    flex-grow: 1;
+    height: 1px;
+    background-color: #ccc;
 }
 
 </style>
