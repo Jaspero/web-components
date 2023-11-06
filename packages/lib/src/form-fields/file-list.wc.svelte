f0f22d43dcfbf26e37fe86e5c226ac0418aaed9f
Marin Kovač <marin.kovac04@gmail.com>
2023-11-06 21:44:56 +0100

    feat: initial commit

---
+++ packages/lib/src/form-fields/file-list.wc.svelte
@@ -0,0 +1,219 @@
+<svelte:options
+  customElement={{
+    tag: 'jp-file-list',
+    shadow: 'none',
+    extend: (customElementConstructor) => {
+      return class extends customElementConstructor {
+        static formAssociated = true;
+        constructor() {
+          super();
+          this.attachedInternals = this.attachInternals();
+        }
+      };
+    }
+  }}
+/>
+
+<script lang="ts">
+  import type FileService from '../types/file.service';
+  import { createEventDispatcher } from 'svelte';
+
+  export let attachedInternals: ElementInternals;
+  export let value: string = '';
+  export let id: string = '';
+  export let name: string = '';
+  let files = [];
+  let fileInputEl;
+  let browseFilesEl;
+  let hoveringFile = false;
+
+  export let service: FileService;
+
+  export const getValue = () => value;
+
+  const dispatch = createEventDispatcher();
+  $: {
+    attachedInternals.checkValidity();
+    attachedInternals.setFormValue(value);
+    dispatch('value', { value });
+  }
+  $: if (fileInputEl) fileInputEl.files = makeFileList(files.map((el) => el.file));
+
+  function handleFileInput(e) {
+    if (e.target.files.length) {
+      files = files.concat(
+        Array.from(e.target.files).map((el) => {
+          let obj = {
+            name: el.name,
+            file: el
+          };
+          if (el['type'].split('/')[0] === 'image') {
+            obj['src'] = URL.createObjectURL(el);
+          }
+          return obj;
+        })
+      );
+    }
+  }
+
+  function handleDrop(e) {
+    if (e.dataTransfer.files.length) {
+      files = files.concat(
+        Array.from(e.dataTransfer.files).map((el) => {
+          let obj = {
+            name: el.name,
+            file: el
+          };
+          if (el['type'].split('/')[0] === 'image') {
+            obj['src'] = URL.createObjectURL(el);
+          }
+          return obj;
+        })
+      );
+      hoveringFile = false;
+    }
+  }
+
+  const makeFileList = (fileArr) => {
+    const reducer = (dataTransfer, file) => {
+      dataTransfer.items.add(file);
+      return dataTransfer;
+    };
+    return fileArr.reduce(reducer, new DataTransfer()).files;
+  };
+
+  export async function save(id?: string) {
+    /*try {
+      value = await service.uploadFile(file, id);
+    } catch (err) {
+      console.log(err);
+    }
+    return value;*/
+  }
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+
+      fileReader.onload = () => resolve(fileReader.result);
+      fileReader.onerror = (err) => reject(err);
+    });
+  };
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+  class="dropzone"
+  class:fullBorder={hoveringFile}
+  on:dragover|preventDefault={() => (hoveringFile = true)}
+>
+  {#if hoveringFile}
+    <div
+      class="info"
+      on:dragleave={() => (hoveringFile = false)}
+      on:dragend={() => (hoveringFile = false)}
+      on:drop|preventDefault={(e) => handleDrop(e)}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"
+        ><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+          d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+        /></svg
+      >
+      <div>Drop your files here</div>
+    </div>
+  {/if}
+  {#if files.length == 0}
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div class="info" hidden={files.length != 0}>
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <div>
+        Drop files here or <a on:click={() => browseFilesEl.click()}>browse files</a>
+      </div>
+    </div>
+  {:else}
+    <div class="files">
+      {#each files as file, index}
+        <div class="file">
+          <button class="file-remove" on:click|preventDefault={() => {files.splice(index, index); files = files}}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>
+          </button>
+          <div class="file-icon">
+            {#if file.src}
+              <img src={file.src} alt={file.name} />
+            {:else}
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"/></svg>
+            {/if}
+          </div>
+          <div class="file-name">
+            {file.name}
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div>
+<input type="file" multiple bind:this={browseFilesEl} on:change={(e) => handleFileInput(e)} hidden />
+<input type="file" {id} {name} bind:this={fileInputEl} hidden />
+
+<style>
+  .dropzone {
+    background-color: #f4f4f4;
+    width: 750px;
+    height: 500px;
+    border: 1px dashed #e6510030;
+  }
+
+  .fullBorder {
+    border: 1px dashed #e65000;
+  }
+
+  .info {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .info a {
+    color: #e65000;
+    cursor: pointer;
+    text-size: 20px;
+  }
+
+  .info a:hover {
+    text-decoration: underline;
+  }
+
+  .files {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .file {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 23%;
+    margin: 0 1%;
+  }
+
+  .file-remove {
+    cursor: pointer;
+  }
+
+  .file-icon {
+    margin: 10px 0;
+    width: 150px;
+    height: 150px;
+  }
+
+  .file-icon img, .file-icon svg {
+    object-fit: cover;
+    min-width: 100%;
+    min-height: 100%
+  }
+</style>
