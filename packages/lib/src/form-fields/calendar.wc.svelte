cff382bff872a735e0d51cfc03f92a0165b2daac
Antonio Ištvanović <87823353+istvavk@users.noreply.github.com>
2023-11-09 17:17:28 +0100

    chore(calendar): delete an event

--- packages/lib/src/form-fields/calendar.wc.svelte
+++ packages/lib/src/form-fields/calendar.wc.svelte
@@ -51,7 +51,7 @@
     'Nov',
     'Dec'
   ];
-  let selectedTime = '';
+  let selectedTime;
   let yearPickerIndex = 0;
   let schedules = [];
 
@@ -147,11 +147,6 @@
   }
 
   function updateScheduleArray() {
-    console.log('yearSelected:', yearSelected);
-    console.log('monthSelected:', monthSelected);
-    console.log('dateSelected:', dateSelected);
-    console.log('selectedTime:', selectedTime);
-
     const selectedDate = new Date(
             `${yearSelected}-${monthSelected + 1 < 10 ? '0' : ''}${monthSelected}-${
                     dateSelected < 10 ? '0' : ''
@@ -164,8 +159,6 @@
       date: selectedDate
     };
     schedules = [...schedules, newSchedule];
-    inputValue = '';
-    selectedTime = null;
     modalOpen = false;
   }
 
@@ -187,6 +180,23 @@
     }
     schedulesByDay = { ...schedulesByDay };
     console.log(schedulesByDay);
+  }
+
+  function deleteModalData (data) {
+    inputValue = '';
+    modalOpen = false;
+    selectedTime = null;
+    if (schedules) {
+        schedules = schedules.filter((el) => {
+            const day = el.date.toISOString().split('T')[0];
+            const selectedDay = new Date(
+                    `${yearSelected}-${monthSelected + 1 < 10 ? '0' : ''}${monthSelected}-${
+                            dateSelected < 10 ? '0' : ''
+                    }${dateSelected}`
+            ).toISOString().split('T')[0];
+            return day !== selectedDay;
+        });
+    }
   }
 </script>
 
@@ -258,7 +268,6 @@
           {/each}
         </tr>
       {/each}
-
     </table>
   </div>
 
@@ -374,8 +383,11 @@
 
 <div class="modal" style={`display: ${modalOpen ? 'flex' : 'none'}`}>
   <div class="modal-container">
-    <h2>Add an event or task</h2>
-    <hr />
+    <div>
+      <svg on:click|preventDefault={() => (modalOpen = false)} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+      <h2>Add an event or task</h2>
+      <hr />
+    </div>
     <input type="text" placeholder="Write a description" bind:value={inputValue} />
     <div>
       <div>
@@ -386,7 +398,7 @@
       </div>
     </div>
     <div>
-      <button class="close-button" on:click|preventDefault={() => (modalOpen = false)}>Close</button
+      <button class="delete-button" on:click|preventDefault={() => deleteModalData()}>Delete</button
       >
       <button
         on:click|preventDefault={() => updateScheduleArray()}
