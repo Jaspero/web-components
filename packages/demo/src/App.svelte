a362e276e84f74e3ad768c872ab9ebbbd4eb11aa
Antonio Ištvanović <87823353+istvavk@users.noreply.github.com>
2023-11-20 14:27:10 +0100

    chore(buttonTooltip): setup

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -1,37 +1,39 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  /*import '../../../dist/alert.wc.js'; */
-  /*import '../../../dist/confirm.wc.js'*/
-  /*import '../../../dist/async-table.wc.js';*/
-  /*import '../../../dist/input.wc.js';
-  /*import '../../../dist/quill.wc.js';*/
-  import '../../../dist/autocomplete.wc.js';
-  import '../../../dist/checkbox.wc.js';
-  /*import '../../../dist/progress-spinner.wc.js'*/
-  import '../../../dist/chips.wc.js';
-  import '../../../dist/datepicker.wc.js';
-  import '../../../dist/multiselect.wc.js';
-  import '../../../dist/select.wc.js';
-  import '../../../dist/accordion.wc.js';
-  import '../../../dist/bellow.wc.js';
-  import '../../../dist/radio.wc.js'
-  import '../../../dist/json-editor.wc.js'
-  import '../../../dist/code-editor.wc.js'
-  /*import '../../../dist/image-gallery.wc.js'
-  import '../../../dist/image-upload.wc.js'
-  import '../../../dist/paginator.wc.js'*/
-  import '../../../dist/table.wc.js'
-  import '../../../dist/textarea.wc.js';
-  import '../../../dist/button.wc.js'
-  import '../../../dist/link.wc.js'
-  import '../../../dist/tabs.wc.js'
-  import '../../../dist/slider.wc.js'
-  import '../../../dist/stepper.wc.js';
-  import '../../../dist/tree.wc.js';
+  // import '../../../dist/alert.wc.js';
+  // /*import '../../../dist/confirm.wc.js'*/
+  // /*import '../../../dist/async-table.wc.js';*/
+  // /*import '../../../dist/input.wc.js';
+  // /*import '../../../dist/quill.wc.js';*/
+  // import '../../../dist/autocomplete.wc.js';
+  // import '../../../dist/checkbox.wc.js';
+  // /*import '../../../dist/progress-spinner.wc.js'*/
+  // import '../../../dist/chips.wc.js';
+  // import '../../../dist/datepicker.wc.js';
+  // import '../../../dist/multiselect.wc.js';
+  // import '../../../dist/select.wc.js';
+  // import '../../../dist/accordion.wc.js';
+  // import '../../../dist/bellow.wc.js';
+  // import '../../../dist/radio.wc.js'
+  // import '../../../dist/json-editor.wc.js'
+  // import '../../../dist/code-editor.wc.js'
+  // /*import '../../../dist/image-gallery.wc.js'
+  // import '../../../dist/image-upload.wc.js'
+  // import '../../../dist/paginator.wc.js'*/
+  // import '../../../dist/table.wc.js'
+  // import '../../../dist/textarea.wc.js';
+  // import '../../../dist/button.wc.js'
+  // import '../../../dist/link.wc.js'
+  // import '../../../dist/tabs.wc.js'
+  // import '../../../dist/slider.wc.js'
+  // import '../../../dist/stepper.wc.js';
+  // import '../../../dist/tree.wc.js';
   import { renderAlert } from '../../../dist/render-alert.js';
   import { renderConfirm } from '../../../dist/render-confirm.js';
   import { FirebaseTableService } from './firebase-table.service';
   import { MockImageService } from './mock-image.service..js';
+  import '../../../dist/buttonTooltipAttribute.wc.js'
+
 
   let el: HTMLDivElement;
   let formEl: HTMLFormElement;
@@ -365,3 +367,6 @@
   <jp-tab title="two">Content 2</jp-tab>
   <jp-tab title="three">Content 3</jp-tab>
 </jp-tabs>
+
+
+<jp-buttonTooltipAttribute></jp-buttonTooltipAttribute>
---
+++ packages/lib/src/form-fields/buttonTooltipAttribute.wc.svelte
@@ -0,0 +1,26 @@
+<svelte:options
+        customElement={{
+    tag: 'jp-buttonTooltipAttribute',
+    shadow: 'none',
+    extend: (customElementConstructor) => {
+      return class extends customElementConstructor {
+        static formAssociated = true;
+
+        constructor() {
+          super();
+          this.attachedInternals = this.attachInternals();
+        }
+      };
+    }
+  }}
+/>
+
+<script lang="ts">
+    export let tooltip = '';
+
+    if (tooltip) {
+        console.log('tooltip', tooltip);
+    }
+</script>
+
+<button class="delete-button" tooltip="Delete">Delete</button>
