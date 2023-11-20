247581aad57f768f980a19abc2969e8543472e36
Marin Kovač <marin.kovac04@gmail.com>
2023-11-20 23:56:50 +0100

    fix: uploadFile not optional (file service)

--- packages/lib/src/types/file.service.ts
+++ packages/lib/src/types/file.service.ts
@@ -1,5 +1,5 @@
 export default interface FileService {
   acceptedFiles: string;
   maxSize?: number;
-  uploadFile?: (file: any, id?: string) => Promise<string>;
+  uploadFile: (file: any, id?: string) => Promise<string>;
 }
