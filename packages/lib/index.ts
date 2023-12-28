aebf21bdcd1572889f93853ecbb3ab101079d744
Marin Kovač <marin.kovac04@gmail.com>
2023-12-28 12:00:40 +0100

    chore: initial multisearch commit

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -21,6 +21,7 @@ export default () => {
   import('./src/form-fields/radio.wc.svelte');
   import('./src/form-fields/autocomplete.wc.svelte');
   import('./src/form-fields/color.wc.svelte');
+  import('./src/form-fields/multisearch.wc.svelte');
   import('./src/editors/json-editor.wc.svelte');
   import('./src/editors/code-editor.wc.svelte');
   import('./src/data-paginator/data-paginator.wc.svelte');
