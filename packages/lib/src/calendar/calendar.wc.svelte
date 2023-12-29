5f4f2b68bd26525bed166da255b0362c451be6d1
Antonio Ištvanović <87823353+istvavk@users.noreply.github.com>
2023-12-29 19:10:24 +0100

    chore(calendar): code rewrite, add an event

--- packages/lib/src/calendar/calendar.wc.svelte
+++ packages/lib/src/calendar/calendar.wc.svelte
@@ -66,6 +66,101 @@
 
   $: pickerYearRows = getYearPickerRows(yearPickerIndex);
 
+  function updateScheduleArray() {
+    const selectedDate = new Date(
+            `${yearSelected}-${monthSelected + 1 < 10 ? '0' : ''}${monthSelected}-${
+                    dateSelected < 10 ? '0' : ''
+            }${dateSelected} ${selectedTime}`
+    );
+    console.log('selectedDate:', selectedDate);
+
+    const newSchedule = {
+      description: inputValue,
+      date: selectedDate
+    };
+    schedules = [...schedules, newSchedule];
+    modalOpen = false;
+  }
+
+  $: if (pickerMonth == 12) {
+    pickerMonth = 0;
+    pickerYear++;
+  } else if (pickerMonth == -1) {
+    pickerMonth = 11;
+    pickerYear--;
+  }
+
+  const getCalendarRows = (year, month) => {
+    const firstDayOfMonth = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(year, month + 1, 0);
+    const daysInMonth = lastDayOfMonth.getDate();
+
+    const rows = [];
+    let currentRow = [];
+    let currentDate = new Date(firstDayOfMonth);
+
+    // previous month
+    for (let i = firstDayOfMonth.getDay() - 1; i >= 0; i--) {
+      const prevMonthDate = new Date(year, month, 0);
+      prevMonthDate.setDate(prevMonthDate.getDate() - i);
+      currentRow.push({
+        year: prevMonthDate.getFullYear(),
+        month: prevMonthDate.getMonth(),
+        day: prevMonthDate.getDate(),
+      });
+    }
+
+    // current month
+    for (let i = 1; i <= daysInMonth; i++) {
+      currentRow.push({ year, month, day: i });
+      if (currentRow.length === 7) {
+        rows.push(currentRow);
+        currentRow = [];
+      }
+    }
+
+    // next month
+    for (let i = 1; currentRow.length < 7; i++) {
+      const nextMonthDate = new Date(year, month + 1, i);
+      currentRow.push({ year: nextMonthDate.getFullYear(), month: nextMonthDate.getMonth(), day: nextMonthDate.getDate() });
+    }
+
+    return rows;
+  };
+
+  $: pickerRows = getCalendarRows(pickerYear, pickerMonth + 1);  // I've added this +1 because of indicies
+
+
+  function hideButton() {
+
+    const selectedDate = new Date(
+            `${yearSelected}-${monthSelected + 1 < 10 ? '0' : ''}${monthSelected}-${
+                    dateSelected < 10 ? '0' : ''
+            }${dateSelected} ${selectedTime}`
+    );
+
+    console.log(selectedDate);
+
+    return !selectedDate;
+  }
+
+  function deleteModalData (data) {
+    inputValue = '';
+    modalOpen = false;
+    selectedTime = null;
+    if (schedules) {
+      schedules = schedules.filter((el) => {
+        const day = el.date.toISOString().split('T')[0];
+        const selectedDay = new Date(
+                `${yearSelected}-${monthSelected + 1 < 10 ? '0' : ''}${monthSelected}-${
+                        dateSelected < 10 ? '0' : ''
+                }${dateSelected}`
+        ).toISOString().split('T')[0];
+        return day !== selectedDay;
+      });
+    }
+  }
+
   function prepareModalData(col) {
     yearSelected = col.year;
     monthSelected = col.month;
@@ -88,6 +183,34 @@
 
 
     modalOpen = true;
+  }
+
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      updateScheduleArray();
+    } else if (event.key === 'Escape') {
+      modalOpen = false;
+    }
+  });
+
+
+  $: {
+    if (schedules) {
+      console.log(1, schedules);
+      schedulesByDay = schedules.reduce((acc, cur) => {
+        const day = cur.date.toISOString().split('T')[0];
+
+        if (!acc[day]) {
+          acc[day] = [];
+        }
+
+        acc[day].push(cur);
+
+        return acc;
+      }, {});
+    }
+    schedulesByDay = { ...schedulesByDay };
+    console.log(schedulesByDay);
   }
 </script>
 
@@ -122,39 +245,37 @@
     </div>
   </div>
 
-  <div>
-    <table>
+  <table>
+    <tr>
+      {#each daysMap as day}
+        <th>
+          {day}
+        </th>
+      {/each}
+    </tr>
+    {#each pickerRows as row}
       <tr>
-        {#each daysMap as day}
-          <th>
-            {day}
-          </th>
-        {/each}
-      </tr>
-      {#each pickerRows as row}
-        <tr>
-          {#each row as col}
-            {@const key = col.year + '-' + col.month + '-' + col.day}
-            <td
-                    on:click={() => {
+        {#each row as col}
+          {@const key = col.year + '-' + col.month + '-' + col.day}
+          <td
+                  on:click={() => {
                 console.log(col);
                 prepareModalData(col);
               }}
-            >
-              <div class="cell-date">
-                {col.day}
-              </div>
-              {#if schedulesByDay[key]}
-                {#each schedulesByDay[key] as event}
-                  <div>{event.description} {event.date.toLocaleString()}</div>
-                {/each}
-              {/if}
-            </td>
-          {/each}
-        </tr>
-      {/each}
-    </table>
-  </div>
+          >
+            <div class="cell-date">
+              {col.day}
+            </div>
+            {#if schedulesByDay[key]}
+              {#each schedulesByDay[key] as event}
+                <div>{event.description} {event.date.toLocaleString()}</div>
+              {/each}
+            {/if}
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </table>
 
 
 
@@ -267,10 +388,41 @@
   {/if}
 </div>
 
+{#if modalOpen}
+  <div class="modal" style="display: flex;">
+    <div class="modal-container">
+      <div>
+        <svg on:click|preventDefault={() => (modalOpen = false)} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+        <h2>Add an event or task</h2>
+        <hr />
+      </div>
+      <input on:submbit type="text" placeholder="Write a description" bind:value={inputValue} />
+      <div>
+        <div>
+          {dateSelected}.{monthMap[monthSelected - 1]}.{yearSelected}
+        </div>
+        <div>
+          <input type="time" bind:value={selectedTime} />
+        </div>
+      </div>
+      <div>
+        {#if hideButton()}
+          <button id="deleteButton" class="delete-button" on:click|preventDefault={() => deleteModalData()}>Delete</button>
+        {/if}
+        <button
+                on:click|preventDefault={() => updateScheduleArray()}
+                disabled={!inputValue || !selectedTime}>Save</button
+        >
+      </div>
+    </div>
+  </div>
+{/if}
+
 
 <style>
   .calendar {
     margin-top: 100px;
+    margin-bottom: 100px;
     border: 1px solid #e65000;
     justify-content: space-between;
     max-width: 900px;
@@ -418,5 +570,22 @@
     position: absolute;
     right: 0;
     top: 0;
+  }
+
+  .modal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10000;
+    background-color: white;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .modal-container {
+    max-width: 400px;
+    margin: 0 auto;
   }
 </style>
