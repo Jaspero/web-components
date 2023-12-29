340ab70125e1d0ec3355f10b33a9be63c3522398
KovaCro <KovaCro@users.noreply.github.com>
2023-12-29 14:12:05 +0000

    deploy: 80848afbb6ce6c641d00e742c858a2d20f11ec48

---
+++ c/types/search.service.ts
@@ -0,0 +1,5 @@
+export default interface SearchService {
+  search: (str: string) => Promise<Array<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>>;
+  loadMore: (str: string) => Promise<Array<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>>;
+  getSingle: (value: string) => Promise<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>;
+}
