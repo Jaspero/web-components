e85323fe8710cb6cac25dbfa5c289dd92abe639c
ZeljkoMutic <zeljkomtc2@gmail.com>
2024-01-03 14:11:49 +0100

    arranging columns + export csv to table

--- packages/lib/src/table/table.wc.svelte
+++ packages/lib/src/table/table.wc.svelte
@@ -1,5 +1,5 @@
 <svelte:options
-  customElement={{
+        customElement={{
     tag: 'jp-table',
     shadow: 'none'
   }}
@@ -10,10 +10,19 @@
   import { get } from '../utils/json-pointer';
   import type { TableHeader } from '../types/table-header.interface';
   import type { TableSort } from '../types/table-sort.interface';
+  import type { TableService } from '../types/table.service';
 
   export let headers: TableHeader[] = [];
   export let rows: any[] = [];
   export let sort: TableSort;
+  export let service: TableService;
+  let exportLoading = false;
+  export let showExport = true;
+  let columnOrder: string[] = [];
+
+  export let showArrangingColumns = true;
+  let arrangingColumns = false;
+  let isDraging = false;
 
   const dispatch = createEventDispatcher();
 
@@ -47,7 +56,7 @@
     }
 
     const direction =
-      sort?.key === header.key ? (sort.direction === 'asc' ? 'desc' : 'asc') : 'asc';
+            sort?.key === header.key ? (sort.direction === 'asc' ? 'desc' : 'asc') : 'asc';
 
     if (sortMethod) {
       rows = [...rows.sort((a, b) => sortMethod(direction, a, b))];
@@ -79,9 +88,7 @@
           case 'number':
             return direction === 'asc' ? aValue - bValue : bValue - aValue;
           case 'string':
-            return direction === 'asc'
-              ? aValue.localeCompare(bValue)
-              : bValue.localeCompare(aValue);
+            return direction === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
           default:
             return 0;
         }
@@ -94,23 +101,135 @@
     };
   }
 
-  function rowClick(row: any, index: number, header: TableHeader) {
+  function rowClick(row: any, index: number, header: TableHeader, event: any) {
     dispatch('rowClick', {
       row,
       index,
-      header
+      header,
+      originalEvent: event
     });
   }
+
+  async function exportData() {
+    exportLoading = true;
+
+    const data = await service.export();
+    const resolved = await Promise.all(
+            data.map(async (row, index) => {
+              const columns = await Promise.all(
+                      activeHeaders.map((header) => handleColumn(header, row, index))
+              );
+
+              return columns.map((col) => `"${col || ''}"`).join(',');
+            })
+    );
+    const blob = new Blob(
+            [
+              [
+                activeHeaders
+                        .map((h) => h.label)
+                        .map((label) => `"${label}"`)
+                        .join(','),
+                ...resolved,
+              ].join('\n'),
+            ],
+            { type: 'text/csv' }
+    );
+    const link = document.createElement('a');
+
+    link.href = URL.createObjectURL(blob);
+    link.download = 'export.csv';
+    link.click();
+
+    exportLoading = false;
+  }
+
+  let activeHeaders: TableHeader[] = [];
+
+  function arrangeColumns() {
+    arrangingColumns = true;
+    columnOrder = activeHeaders.map((header) => header.key);
+  }
+
+  function finishArrangingColumns() {
+    arrangingColumns = false;
+  }
+
+  function dragstart(event: DragEvent, header: TableHeader) {
+    event.dataTransfer.setData('text/plain', header.key);
+    isDraging = true;
+  }
+
+  function dragover(event: DragEvent) {
+    event.preventDefault();
+    isDraging = false;
+  }
+
+  function drop(event: DragEvent) {
+    event.preventDefault();
+    const draggedColumn = event.dataTransfer.getData('text/plain');
+    const currentIndex = columnOrder.indexOf(draggedColumn);
+    const targetIndex = (event.target as HTMLElement)?.dataset.index;
+
+    if (currentIndex !== -1 && targetIndex) {
+      columnOrder.splice(currentIndex, 1);
+      columnOrder.splice(Number(targetIndex), 0, draggedColumn);
+
+      activeHeaders = headers.filter((it) => !it.disabled).sort((a, b) => {
+        const aIndex = columnOrder.indexOf(a.key);
+        const bIndex = columnOrder.indexOf(b.key);
+        return aIndex - bIndex;
+      });
+    }
+  }
+
+  $: activeHeaders = headers.filter((it) => !it.disabled);
 </script>
 
+
 <div class="table-card">
+  {#if showArrangingColumns || showExport}
+    <div class="table-header">
+      {#if showArrangingColumns}
+        {#if !arrangingColumns}
+          <button type="button" on:click={arrangeColumns} class="table-button settings-button">
+            Arrange Columns
+          </button>
+        {:else}
+          <button type="button" on:click={finishArrangingColumns} class="table-button settings-button">
+            Finish Arranging
+          </button>
+        {/if}
+      {/if}
+      {#if showExport}
+        &nbsp;
+        <button
+                type="button"
+                class="table-button settings-button"
+                on:click={exportData}
+                class:loading={exportLoading}
+        >
+          Export
+        </button>
+      {/if}
+    </div>
+  {/if}
   <div class="table-container">
     <table>
-      {#if headers}
+      {#if activeHeaders}
         <tr>
-          {#each headers as header}
-            <th class:sortable={header.sortable} on:click={() => adjustSort(header)}>
-              <span>{@html header.label}</span>
+          {#each activeHeaders as header, index}
+            <th
+                    class:sortable={header.sortable}
+                    on:click={() => adjustSort(header)}
+                    on:drop={drop}
+                    on:dragover={dragover}
+                    data-index={index}
+            >
+              <span draggable="true"  on:dragstart={(e) => dragstart(e, header)}>
+                {@html header.label}
+              </span>
+
               {#if sort?.key === header.key}
                 <span>{sort.direction === 'asc' ? '↑' : '↓'}</span>
               {/if}
@@ -122,8 +241,8 @@
       {#if rows}
         {#each rows as row, index}
           <tr>
-            {#each headers as header}
-              <td on:click={() => rowClick(row, index, header)}>
+            {#each activeHeaders as header}
+              <td on:click={(e) => rowClick(row, index, header, e)}>
                 {#await handleColumn(header, row, index) then val}
                   <span class="cell">
                     {@html val}
@@ -141,7 +260,11 @@
 <style>
   .table-card {
     background-color: var(--background-primary);
+    -webkit-border-radius: 0.25rem;
+    -moz-border-radius: 0.25rem;
     border-radius: 0.25rem;
+    -webkit-box-shadow: 0 3px 12px rgba(0, 0, 0, 0.16);
+    -moz-box-shadow: 0 3px 12px rgba(0, 0, 0, 0.16);
     box-shadow: 0 3px 12px rgba(0, 0, 0, 0.16);
   }
 
@@ -173,5 +296,128 @@
     max-width: 64ch;
     overflow: hidden;
     text-overflow: ellipsis;
+  }
+
+  .table-actions {
+    padding: 1rem;
+  }
+
+  .table-header {
+    padding: 1rem;
+    min-height: 68px;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .table-button {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -moz-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -moz-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-border-radius: 0.25rem;
+    -moz-border-radius: 0.25rem;
+    border-radius: 0.25rem;
+    min-width: 4rem;
+    height: 2.25rem;
+    padding: 0 1rem;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .settings-button {
+    background-color: var(--background-secondary);
+    color: var(--text-on-secondary);
+  }
+
+  .load-button {
+    background-color: var(--primary-color);
+    color: var(--text-on-primary);
+  }
+
+  .load-button:disabled {
+    opacity: 0.5;
+  }
+
+  .load-button.loading {
+    pointer-events: none;
+  }
+
+  .spinner {
+    display: block;
+    border-top: 2px solid var(--text-on-primary);
+    border-bottom: 2px solid var(--text-on-primary);
+    border-left: 2px solid transparent;
+    border-right: 2px solid transparent;
+    -webkit-border-radius: 50%;
+    -moz-border-radius: 50%;
+    border-radius: 50%;
+    width: 1rem;
+    height: 1rem;
+    margin-right: 0.5rem;
+    -webkit-animation: spin 1s linear infinite;
+    -moz-animation: spin 1s linear infinite;
+    -o-animation: spin 1s linear infinite;
+    animation: spin 1s linear infinite;
+  }
+
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+
+  @-moz-keyframes spin {
+    0% {
+      -moz-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -moz-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+
+  @-o-keyframes spin {
+    0% {
+      -o-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -o-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
   }
 </style>
