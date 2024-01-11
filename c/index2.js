a5ffe2dacbd5ba281f02bae6fcca00f213ad7ba9
flauc <flauc@users.noreply.github.com>
2024-01-11 07:44:37 +0000

    deploy: 10728c2a014a5ae9e1b2da25061fb3e1f46a8ce3

--- c/index2.js
+++ c/index2.js
@@ -1,1 +1,1 @@
-import{$ as e}from"./svelte.js";typeof window<"u"&&(window.__svelte||(window.__svelte={v:new Set})).v.add(e);
+import{a0 as e}from"./svelte.js";typeof window<"u"&&(window.__svelte||(window.__svelte={v:new Set})).v.add(e);
--- c/types/search.service.ts
+++ c/types/search.service.ts
@@ -1,5 +1,11 @@
 export default interface SearchService {
-  search: (str: string) => Promise<Array<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>>;
-  loadMore: (str: string) => Promise<Array<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>>;
-  getSingle: (value: string) => Promise<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>;
+  search: (
+    str: string
+  ) => Promise<Array<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>>;
+  loadMore: (
+    str: string
+  ) => Promise<Array<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>>;
+  getSingle: (
+    value: string
+  ) => Promise<{ label?: string; value: string; selected?: boolean; disabled?: boolean }>;
 }
