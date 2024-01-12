a9d1807c189154364b38273269c6e604cbaeb933
Marin Kovač <marin.kovac04@gmail.com>
2024-01-12 18:29:47 +0100

    Revert "attempt at making schedules display in calendar"
    This reverts commit d0dd59b639d8ada3bc9732abc5ea4fa5971a3f8d.

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,36 +1,36 @@
 export default () => {
-  // import('./src/table/table.wc.svelte');
-  // import('./src/async-table/async-table.wc.svelte');
-  // import('./src/button/button.wc.svelte');
-  // import('./src/link/link.wc.svelte');
-  // import('./src/accordion/accordion.wc.svelte');
-  // import('./src/accordion/bellow.wc.svelte');
-  // import('./src/form-fields/file-upload.wc.svelte');
-  // import('./src/carousel/carousel.wc.svelte');
-  // import('./src/alert/alert.wc.svelte');
-  // import('./src/alert/render-alert');
-  // import('./src/confirm/confirm.wc.svelte');
-  // import('./src/confirm/render-confirm');
-  // import('./src/progress-spinner/progress-spinner.wc.svelte');
-  // import('./src/form-fields/input.wc.svelte');
-  // import('./src/form-fields/quill.wc.svelte');
-  // import('./src/form-fields/checkbox.wc.svelte');
-  // import('./src/form-fields/chips.wc.svelte');
-  // import('./src/form-fields/multiselect.wc.svelte');
-  // import('./src/form-fields/select.wc.svelte');
-  // import('./src/form-fields/radio.wc.svelte');
-  // import('./src/form-fields/autocomplete.wc.svelte');
-  // import('./src/editors/json-editor.wc.svelte');
-  // import('./src/editors/code-editor.wc.svelte');
-  // import('./src/data-paginator/data-paginator.wc.svelte');
-  // import('./src/page-paginator/page-paginator.wc.svelte');
-  // import('./src/form-fields/textarea.wc.svelte');
-  // import('./src/form-fields/datepicker.wc.svelte');
-  // import('./src/form-fields/toggle.wc.svelte')
-  // import('./src/tabs/tabs.wc.svelte')
-  // import('./src/stepper/stepper.wc.svelte')
-  // import('./src/tree/tree.wc.svelte')
-  // import('./src/tree/node.wc.svelte')
-  // import('./src/form-fields/slider.wc.svelte');
-  import('./src/calendar/calendar.wc.svelte');
+  import('./src/table/table.wc.svelte');
+  import('./src/async-table/async-table.wc.svelte');
+  import('./src/button/button.wc.svelte');
+  import('./src/link/link.wc.svelte');
+  import('./src/accordion/accordion.wc.svelte');
+  import('./src/accordion/bellow.wc.svelte');
+  import('./src/form-fields/file-upload.wc.svelte');
+  import('./src/carousel/carousel.wc.svelte');
+  import('./src/alert/alert.wc.svelte');
+  import('./src/alert/render-alert');
+  import('./src/confirm/confirm.wc.svelte');
+  import('./src/confirm/render-confirm');
+  import('./src/progress-spinner/progress-spinner.wc.svelte');
+  import('./src/form-fields/input.wc.svelte');
+  import('./src/form-fields/quill.wc.svelte');
+  import('./src/form-fields/checkbox.wc.svelte');
+  import('./src/form-fields/chips.wc.svelte');
+  import('./src/form-fields/multiselect.wc.svelte');
+  import('./src/form-fields/select.wc.svelte');
+  import('./src/form-fields/radio.wc.svelte');
+  import('./src/form-fields/autocomplete.wc.svelte');
+  import('./src/editors/json-editor.wc.svelte');
+  import('./src/editors/code-editor.wc.svelte');
+  import('./src/data-paginator/data-paginator.wc.svelte');
+  import('./src/page-paginator/page-paginator.wc.svelte');
+  import('./src/form-fields/textarea.wc.svelte');
+  import('./src/form-fields/datepicker.wc.svelte');
+  import('./src/form-fields/toggle.wc.svelte')
+  import('./src/tabs/tabs.wc.svelte')
+  import('./src/stepper/stepper.wc.svelte')
+  import('./src/tree/tree.wc.svelte')
+  import('./src/tree/node.wc.svelte')
+  import('./src/form-fields/slider.wc.svelte');
+  import('./src/form-fields/calendar.wc.svelte');
 };
