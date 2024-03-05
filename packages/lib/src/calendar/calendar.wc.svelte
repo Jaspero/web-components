23c77b215f8c743a92024c64aab1fd2e6cef61e0
Antonio Ištvanović <87823353+istvavk@users.noreply.github.com>
2024-03-05 12:14:03 +0100

    chore(calendar): style

--- packages/lib/src/calendar/calendar.wc.svelte
+++ packages/lib/src/calendar/calendar.wc.svelte
@@ -257,7 +257,7 @@
                                 {col.day}
                             </div>
                             {#each pickerSchedules[i][j] as sch}
-                                <div on:click={() => {
+                                <div class="cell-content" on:click={() => {
                                       inputValue = sch.description;
                                       selectedTime = sch.date.toTimeString().slice(0, 5);
                                       modalOpen = true;
@@ -399,10 +399,19 @@
   }}
 >
     <div class="modal-container" use:clickOutside on:click_outside={() => (modalOpen = false)}>
-        <h2>Create task or event</h2>
-        <hr />
-        <input type="text" placeholder="Write a description" bind:value={inputValue} />
-        <div>
+        <div class="x-mark">
+            <button on:click={() => modalOpen = false}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                </svg>
+            </button>
+        </div>
+        <div class="first-block">
+            <h2>Create task or event</h2>
+            <input type="text" placeholder="Write a description" bind:value={inputValue} />
+        </div>
+        <div class="second-block">
             <div>
                 {dateSelected}.{monthMap[monthSelected]}.{yearSelected}
             </div>
@@ -588,13 +597,34 @@
     }
 
     .cell-date {
+        display: inline-block;
         position: absolute;
         right: 0;
         top: 0;
     }
 
     .x-mark {
-        height: 1.5rem;
-        cursor: pointer;
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    .first-block {
+        margin-bottom: 10px;
+    }
+
+    .second-block {
+        margin-bottom: 10px;
+    }
+
+    .cell-content {
+        /*bottom: 1em;*/
+        display: inline-block;
+        background-color: rgb(3, 155, 229);
+        /*border: 1px solid;*/
+        height: 22px;
+        padding: 0 8px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
     }
 </style>
