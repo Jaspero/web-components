cdeab7c4d6d955a2581d81ccf87c3d90ae86067a
flauc <flauc@users.noreply.github.com>
2024-03-29 12:01:17 +0000

    deploy: b3b0129c0e5663f938b04ed240a683cdb2789c8d

---
+++ c/types/asset-manager.service.ts
@@ -0,0 +1,31 @@
+import type { Writable } from 'svelte/store';
+
+export type Item = Asset | Folder;
+
+export interface Folder {
+  name: string;
+  id: string;
+  type: string;
+}
+
+export interface Asset {
+  id: string;
+  name: string;
+  url: string;
+  contentType: string;
+  status?: Writable<'uploading' | 'uploaded' | 'paused' | 'error'>;
+  progress?: Writable<number>;
+  uploader?: {
+    pause: () => void;
+    resume: () => void;
+    cancel: () => void;
+  };
+  type: string;
+  size: number;
+}
+
+export interface AssetManagerService {
+  fetch: (path: string) => Promise<Item[]>;
+  upload: (path: string, file: any) => Promise<Asset>;
+  remove: (id: string) => Promise<void>;
+}
