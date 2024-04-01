4098b902f1c6e1e792c970654f666cf17db6ebbc
flauc <filip.lauc93@gmail.com>
2024-04-01 22:42:21 +0200

    feat(asset-manager): filtering by asset type

--- packages/lib/src/asset-manager/asset-manager.wc.svelte
+++ packages/lib/src/asset-manager/asset-manager.wc.svelte
@@ -1,5 +1,5 @@
 <svelte:options
-        customElement={{
+  customElement={{
     tag: 'jp-asset-manager',
     shadow: 'none'
   }}
@@ -9,11 +9,12 @@
   import type { AssetManagerService, Item } from '../types/asset-manager.service';
   import Folder from './Folder.svelte';
   import Asset from './Asset.svelte';
-  import {createEventDispatcher} from 'svelte';
+  import { createEventDispatcher } from 'svelte';
 
   export let rootPath = '/';
   export let maxSize = 10 * 1048576;
   export let acceptedFiles = '*';
+  export let shownFiles = '*';
   export let service: AssetManagerService;
   export let selectable: '' | 'single' | 'multiple' = '';
 
@@ -21,7 +22,7 @@
 
   let path = rootPath;
   let items: Item[] = [];
-  let selectedItems: {[key: string]: Asset} = {};
+  let selectedItems: { [key: string]: Asset } = {};
   let browseFilesEl: HTMLInputElement;
   let loading = false;
   let hoveringFile = false;
@@ -35,10 +36,7 @@
 
   async function handleFileInput(e) {
     if (e.target.files.length) {
-      items = [
-        ...items,
-        ...await filesToItems(e.target.files)
-      ]
+      items = [...items, ...(await filesToItems(e.target.files))];
     }
 
     browseFilesEl.value = null;
@@ -46,10 +44,7 @@
 
   async function handleDrop(e) {
     if (e.dataTransfer.files.length) {
-      items = [
-        ...items,
-        ...await filesToItems(e.dataTransfer.files)
-      ]
+      items = [...items, ...(await filesToItems(e.dataTransfer.files))];
     }
 
     hoveringFile = false;
@@ -57,19 +52,18 @@
 
   function filesToItems(files: FileList) {
     return Promise.all(
-            Array.from(files)
-                    .filter(file => {
+      Array.from(files)
+        .filter((file) => {
+          // TODO: Show alert for each file violating file size
+          if (!maxSize || maxSize < file.size) {
+            return false;
+          }
 
-                      // TODO: Show alert for each file violating file size
-                      if (!maxSize || maxSize < file.size) {
-                        return false;
-                      }
-
-                      return true;
-                    })
-                    .map(file => service.upload(path, file))
+          return true;
+        })
+        .map((file) => service.upload(path, file))
     );
-  };
+  }
 
   function back() {
     path = path.split('/').slice(0, -1).join('/');
@@ -111,28 +105,36 @@
   $: if (path) {
     loading = true;
     service
-            .fetch(path)
-            .then((data) => {
-              items = data;
-            })
-            .finally(() => {
-              loading = false;
-            });
+      .fetch(path)
+      .then((data) => {
+        items = data;
+
+        if (shownFiles && shownFiles !== '*') {
+          // @ts-ignore
+          items = items.filter(item => item.type === 'folder' || (item as Asset).contentType.startsWith(shownFiles));
+        }
+      })
+      .finally(() => {
+        loading = false;
+      });
   }
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<section class="card" class:full-border={hoveringFile}
-         on:dragover|preventDefault={() => (hoveringFile = true)}
-         on:dragleave={() => (hoveringFile = false)}
-         on:dragend={() => (hoveringFile = false)}
-         on:drop|preventDefault={(e) => handleDrop(e)}>
-
-
+<section
+  class="card"
+  class:full-border={hoveringFile}
+  on:dragover|preventDefault={() => (hoveringFile = true)}
+  on:dragleave={() => (hoveringFile = false)}
+  on:dragend={() => (hoveringFile = false)}
+  on:drop|preventDefault={(e) => handleDrop(e)}
+>
   {#if hoveringFile}
     <div class="drop-here">
       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 384 512">
-        <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/>
+        <path
+          d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+        />
       </svg>
       <div>Drop your files here</div>
     </div>
@@ -151,13 +153,21 @@
       <div class="header-actions">
         <button type="button" title="Add Folder" on:click|preventDefault={addFolder}>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -960 960 960">
-            <path d="M560-320h80v-80h80v-80h-80v-80h-80v80h-80v80h80v80ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z"/>
+            <path
+              d="M560-320h80v-80h80v-80h-80v-80h-80v80h-80v80h80v80ZM160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Z"
+            />
           </svg>
         </button>
 
-        <button type="button" title="Add Files" on:click|preventDefault={() => browseFilesEl.click()}>
+        <button
+          type="button"
+          title="Add Files"
+          on:click|preventDefault={() => browseFilesEl.click()}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -960 960 960">
-            <path d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/>
+            <path
+              d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"
+            />
           </svg>
         </button>
       </div>
@@ -175,10 +185,21 @@
       <div class="files">
         {#each items as item, index}
           {#if item.type === 'folder'}
-            <Folder folder={item} bind:path={path} />
+            <Folder folder={item} bind:path />
           {:else}
-            <div class="asset-button" role="button" tabindex="1" class:selected={selectedItems[item.id]} on:click={() => select(item)}>
-              <Asset asset={item} service={service} on:remove={() => removeFile(index, item.id)} on:cancel={() => cancelUpload(item.id)} />
+            <div
+              class="asset-button"
+              role="button"
+              tabindex="1"
+              class:selected={selectedItems[item.id]}
+              on:click={() => select(item)}
+            >
+              <Asset
+                asset={item}
+                {service}
+                on:remove={() => removeFile(index, item.id)}
+                on:cancel={() => cancelUpload(item.id)}
+              />
             </div>
           {/if}
         {/each}
@@ -193,12 +214,14 @@
   {/if}
 </section>
 
-<input type="file"
-       multiple
-       accept={acceptedFiles}
-       bind:this={browseFilesEl}
-       on:change={(e) => handleFileInput(e)}
-       hidden />
+<input
+  type="file"
+  multiple
+  accept={acceptedFiles}
+  bind:this={browseFilesEl}
+  on:change={(e) => handleFileInput(e)}
+  hidden
+/>
 
 {#if folderDialog}
   <button type="button" on:click={() => (folderDialog = false)}>Close</button>
@@ -221,7 +244,7 @@
     width: 100%;
     height: 500px;
     overflow: auto;
-    box-shadow: 0 3px 6px rgba(0,0,0,.12);
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.12);
     font-family: sans-serif;
   }
 
@@ -233,7 +256,7 @@
     justify-content: space-between;
     background-color: white;
     padding: 20px;
-    border-bottom: 1px solid rgba(0,0,0,.12);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
   }
 
   header .route {
@@ -257,21 +280,21 @@
     width: 40px;
     height: 40px;
     background-color: transparent;
-    border: 1px solid rgba(0,0,0,.12);
+    border: 1px solid rgba(0, 0, 0, 0.12);
     border-radius: 12px;
     cursor: pointer;
-    transition: .25s;
+    transition: 0.25s;
   }
 
   header button:disabled {
-    opacity: .5;
-    background-color: rgba(0,0,0,.1);
+    opacity: 0.5;
+    background-color: rgba(0, 0, 0, 0.1);
     pointer-events: none;
   }
 
   header button:hover {
-    background-color: rgba(0,0,0,.08);
-    border-color: rgba(0,0,0,.2);
+    background-color: rgba(0, 0, 0, 0.08);
+    border-color: rgba(0, 0, 0, 0.2);
   }
 
   .drop-here {
@@ -286,7 +309,7 @@
   :global(.file) {
     position: relative;
     height: 100%;
-    border: 1px solid rgba(0,0,0,.12);
+    border: 1px solid rgba(0, 0, 0, 0.12);
     border-radius: 12px;
     background-color: white;
     overflow: hidden;
@@ -312,11 +335,11 @@
     background-color: white;
     border-radius: 8px;
     cursor: pointer;
-    border: 1px solid rgba(0,0,0,.08);
+    border: 1px solid rgba(0, 0, 0, 0.08);
   }
 
   :global(.file .file-remove:hover) {
-    border-color: rgba(0,0,0,.4);
+    border-color: rgba(0, 0, 0, 0.4);
   }
 
   :global(.file .file-icon-image) {
@@ -325,7 +348,7 @@
     object-fit: contain;
     user-select: none;
     pointer-events: none;
-    background-color: rgba(0,0,0,.02);
+    background-color: rgba(0, 0, 0, 0.02);
   }
 
   .files {
@@ -337,14 +360,14 @@
   .asset-button {
     width: 25%;
     padding: 8px;
-    transition: .25s;
+    transition: 0.25s;
     border-radius: 12px;
     cursor: pointer;
   }
 
   :global(.asset-button:hover .file) {
-    background-color: rgba(0,0,0,.08);
-    border-color: rgba(0,0,0,.2);
+    background-color: rgba(0, 0, 0, 0.08);
+    border-color: rgba(0, 0, 0, 0.2);
   }
 
   :global(.folder) {
@@ -361,16 +384,16 @@
     align-items: center;
     gap: 6px;
     background-color: transparent;
-    border: 1px solid rgba(0,0,0,.12);
+    border: 1px solid rgba(0, 0, 0, 0.12);
     font-size: 14px;
     border-radius: 12px;
     cursor: pointer;
-    transition: .25s;
+    transition: 0.25s;
   }
 
   :global(.folder button:hover) {
-    background-color: rgba(0,0,0,.08);
-    border-color: rgba(0,0,0,.2);
+    background-color: rgba(0, 0, 0, 0.08);
+    border-color: rgba(0, 0, 0, 0.2);
   }
 
   .loader {
@@ -387,8 +410,8 @@
     border-radius: 50%;
     border: 9px solid var(--primary-color);
     animation:
-            spinner-bulqg1 0.8s infinite linear alternate,
-            spinner-oaa3wk 1.6s infinite linear;
+      spinner-bulqg1 0.8s infinite linear alternate,
+      spinner-oaa3wk 1.6s infinite linear;
   }
 
   @keyframes spinner-bulqg1 {
