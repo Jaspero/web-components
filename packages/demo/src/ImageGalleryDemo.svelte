16ec4edbd239b9c364875df3748f61d4c4cf8665
flauc <filip.lauc93@gmail.com>
2024-06-07 20:44:12 +0200

    chore: added demo for image-gallery

---
+++ packages/demo/src/ImageGalleryDemo.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../../../dist/image-gallery.wc.js';
+
+	let el: HTMLDivElement;
+
+	$: if (el) {
+		const imgEl = document.createElement('jp-image-gallery');
+
+		// @ts-ignore
+		imgEl.images = [
+			'https://placehold.co/600x400.png',
+			'https://loremflickr.com/640/360',
+			'https://loremflickr.com/640/360',
+			'https://loremflickr.com/640/360'
+		]
+
+		el.appendChild(imgEl);
+	}
+</script>
+
+<div bind:this={el}></div>
