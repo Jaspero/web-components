d7ce77f06cdfd493e3784f95b05be8110ab8e670
flauc <filip.lauc93@gmail.com>
2024-06-20 15:18:32 +0200

    chore: minor formating

--- packages/demo/src/ImageGalleryDemo.svelte
+++ packages/demo/src/ImageGalleryDemo.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	import '../../../dist/image-gallery.wc.js';
+  import '../../../dist/image-gallery.wc.js';
 
-	let el: HTMLDivElement;
+  let el: HTMLDivElement;
 
-	$: if (el) {
-		const imgEl = document.createElement('jp-image-gallery');
+  $: if (el) {
+    const imgEl = document.createElement('jp-image-gallery');
 
-		// @ts-ignore
-		imgEl.images = [
-			{ src: "https://picsum.photos/200/300" },
-      		{ src: "https://picsum.photos/800/400" },
-      		{ src: "https://picsum.photos/300/300" },
-			{ src: "https://picsum.photos/600/700" },
-      		{ src: "https://picsum.photos/500/500" },
-			{ src: "https://picsum.photos/200/300" },
-      		{ src: "https://picsum.photos/800/400" },
-      		{ src: "https://picsum.photos/300/300" },
-			{ src: "https://picsum.photos/600/700" },
-      		{ src: "https://picsum.photos/500/500" },
-      		{ src: "https://picsum.photos/300/300" }
-		]
+    // @ts-ignore
+    imgEl.images = [
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/300/300' }
+    ];
 
-		el.appendChild(imgEl);
-	}
+    el.appendChild(imgEl);
+  }
 </script>
 
 <div bind:this={el}></div>
