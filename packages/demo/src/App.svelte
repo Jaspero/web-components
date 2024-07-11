3c2d407fad5ca64b089b0bb30d7843f39fc0d8e9
kristian <kristiantarnaj@gmail.com>
2024-07-11 14:28:59 +0200

    feat(components): enabled media preview in ckeditor config

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -2,11 +2,13 @@
   import '../../../dist/checkbox.wc';
   import '../../../dist/input.wc';
   import '../../../dist/select.wc';
+  import '../../../dist/ckeditor.wc';
   import MultiSearchDemo from './MultiSearchDemo.svelte';
   import '../../lib/src/index.css';
 </script>
 
 
+<jp-ckeditor></jp-ckeditor>
 <jp-input label='Input'>
 </jp-input>
 <MultiSearchDemo />
