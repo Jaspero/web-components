7fe59106b4d70f0197445a41b6ef0344793ca853
floyyd6 <slobodantomic82@gmail.com>
2024-07-15 16:29:03 +0200

    fix: added counter, fixed scrolling and closing

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import '../../../dist/checkbox.wc';
+  //import '../../../dist/checkbox.wc';
   import '../../../dist/input.wc';
   import '../../../dist/select.wc';
   import '../../../dist/ckeditor.wc';
@@ -7,8 +7,13 @@
   import '../../lib/src/index.css';
   import AssetManagerDemo from './AssetManagerDemo.svelte';
   import AsyncTableDemo from './AsyncTableDemo.svelte';
+ //import ImageGallery from '../../lib/src/image-gallery/image-gallery.wc.svelte';
+  import ImageGalleryDemo from './ImageGalleryDemo.svelte';
 </script>
 
 <!-- <MultiSearchDemo /> -->
 <!-- <AssetManagerDemo /> -->
-<AsyncTableDemo />
+<!-- AsyncTableDemo /> -->
+ <ImageGalleryDemo /> 
+
+
