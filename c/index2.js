2d7d24a802597ad50b741355276dd1fe0c8e467b
flauc <flauc@users.noreply.github.com>
2024-07-26 22:17:37 +0000

    deploy: da2e8800b8d62d1e8e44b2e38b0190ff2a59a5a7

--- c/index2.js
+++ c/index2.js
@@ -1,1 +1,1 @@
-import{a0 as e}from"./svelte.js";typeof window<"u"&&(window.__svelte||(window.__svelte={v:new Set})).v.add(e);
+import{a3 as e}from"./svelte.js";typeof window<"u"&&(window.__svelte||(window.__svelte={v:new Set})).v.add(e);
