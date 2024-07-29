ea43b65eda396fd8d4c6e7c220cb0c3028dd3ca4
lorka1 <lorka1@users.noreply.github.com>
2024-07-29 07:09:39 +0000

    deploy: 154c90ef9de35f9df92794e4fd86a4c74d363137

--- c/index2.js
+++ c/index2.js
@@ -1,1 +1,1 @@
-import{a3 as e}from"./svelte.js";typeof window<"u"&&(window.__svelte||(window.__svelte={v:new Set})).v.add(e);
+import{a6 as e}from"./svelte.js";typeof window<"u"&&(window.__svelte||(window.__svelte={v:new Set})).v.add(e);
--- c/types/table.service.ts
+++ c/types/table.service.ts
@@ -4,8 +4,10 @@ import type { TableSort } from './table-sort.interface';
 export interface TableService<T = any> {
   get: (sort?: TableSort, pageSize?: number) => Promise<{ hasMore: boolean; rows: T[] }>;
   loadMore: (sort?: TableSort, pageSize?: number) => Promise<{ hasMore: boolean; rows: T[] }>;
-  arrangeColumns?: (headers: TableHeader[]) => TableHeader[];
+  arrangeColumns?: (id: string, headers: string[]) => Promise<void>;
+  getColumnOrder?: (id: string) => Promise<string[]>;
   adjustPageSize?: (pageSize: number) => Promise<void>;
   adjustSort?: (sort: TableSort) => Promise<void>;
   export?: () => Promise<T[]>;
+  import?: (file: File) => Promise<T[]>;
 }
