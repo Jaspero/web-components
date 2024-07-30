9836dbd30d8fb29e190a69ec542b08faae47b7d3
lorka1 <lorena126os@gmail.com>
2024-07-30 16:16:13 +0200

    fix(): fixed required attribute and picker functionality in date picker

--- packages/lib/src/form-fields/datepicker/Day.svelte
+++ packages/lib/src/form-fields/datepicker/Day.svelte
@@ -2,16 +2,16 @@
   import { isOutOfMaxBounds } from './is-out-of-max-bounds.js';
   import { isOutOfMinBounds } from './is-out-of-min-bounds.js';
   import { createEventDispatcher } from 'svelte';
-  export let col: { year: number; month: number; day: number; gray: boolean };
+  export let col: { year: number; month: number; day: number; gray: boolean; selected: boolean };
   export let internalMinDate: Date;
   export let internalMaxDate: Date;
   export let dateSelected: number;
   export let monthSelected: number;
   export let yearSelected: number;
   export let enableMultiple: boolean;
+  export let selectedDates = [];
 
   let isDatePicked = false;
-  let selectedDates = [];
   let date = null;
 
   $: isOutOfMax = isOutOfMaxBounds(internalMaxDate, col.year, col.month, col.day);
@@ -42,17 +42,13 @@
     <button
       type="button"
       class:gray={col.gray}
-      class:active={selectedDates.some(
-        (e) => e.year === col.year && e.month === col.month && e.day === col.day
-      )}
+      class:active={selectedDates.some((e) => e.year === col.year && e.month === col.month && e.day === col.day)}
       on:click|preventDefault={() => {
         dateSelected = col.day;
         yearSelected = col.year;
         monthSelected = col.month;
         date = { year: col.year, month: col.month, day: col.day };
-        const index = selectedDates.findIndex(
-          (e) => e.year === col.year && e.month === col.month && e.day === col.day
-        );
+        const index = selectedDates.findIndex((e) => e.year === col.year && e.month === col.month && e.day === col.day);
         if (index !== -1) {
           isDatePicked = false;
           selectedDates = [...selectedDates.slice(0, index), ...selectedDates.slice(index + 1)];
