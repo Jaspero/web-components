0635f47d416fbd3cf3b385be2812346e81af6d4d
AnteSuvala <antejunior2003@gmail.com>
2024-09-23 18:56:17 +0200

    resolved conflicts

--- packages/lib/src/form-fields/file-list/file-list.wc.svelte
+++ packages/lib/src/form-fields/file-list/file-list.wc.svelte
@@ -18,11 +18,13 @@
   import { createEventDispatcher } from 'svelte';
   import type FileService from '../../types/file.service';
   import Cropper from '../../editors/Cropper.wc.svelte';
+  import { formatDisplayFileName } from '../../utils/fileNameFormatter';
 
   export let wording = {
     DROP_YOUR_FILES_HERE: 'Drop your files here',
     BROWSE_FILES: 'Browse files',
-    DROP_FILES_HERE_OR: 'Drop files here or'
+    DROP_FILES_HERE_OR: 'Drop files here or',
+    SAVED: 'saved'
   };
   export let attachedInternals: ElementInternals;
   export let value = '';
@@ -37,6 +39,10 @@
   export let validationMessages: { [key: string]: string } = {};
   export let sortable = true;
   export let required = false;
+  export let displayFormat = 'snake';
+  export let displayFormatFunction;
+
+  let displayedFileNameString = '';
   let grabbedEl = null;
   let grabbedIndex = -1;
   let startingY: number;
@@ -68,6 +74,7 @@
       .filter((el) => el.saved)
       .map((el) => el.url)
       .join(',');
+
     if (minfiles && internalFiles.length < minfiles) {
       attachedInternals.setValidity(
         { customError: true },
@@ -164,11 +171,17 @@
         } else return true;
       })
       .map((el) => {
+        displayedFileNameString = formatDisplayFileName(
+          el.name,
+          displayFormat,
+          displayFormatFunction
+        );
         let obj: any = {
           name: el.name,
           size: returnFileSize(el.size),
           file: el,
-          saved: false
+          saved: false,
+          displayedName: displayedFileNameString
         };
 
         const type = el['type'].split('/')[0];
@@ -192,35 +205,40 @@
 
     const urls = value.split(',');
 
-    await Promise.allSettled(
-      urls.map(async (url) => {
-        const res = await fetch(url);
-        const urlFile = blobToFile(res.blob(), url);
-        let obj: any = {
-          name: urlFile.name,
-          size: '',
-          file: urlFile,
-          saved: true,
-          url: url,
-          external: true
-        };
+    internalFiles = (
+      await Promise.all(
+        urls.map(async (url) => {
+          try {
+            const res = await fetch(url);
+            const urlFile = blobToFile(res.blob(), url);
+            let obj: any = {
+              name: urlFile.name,
+              displayedName: urlFile.name,
+              size: '',
+              file: urlFile,
+              saved: true,
+              url: url,
+              external: true
+            };
 
-        if (/\.jpg|\.png|\.jpeg|\.webp|\.svg|\.gif/.test(url)) {
-          obj.src = url;
-          obj.type = 'image';
-        } else if (/\.mp4|\.mov/.test(url)) {
-          obj.src = url;
-          obj.type = 'video';
-        } else if (/\.mp3|\.wav/.test(url)) {
-          obj.src = url;
-          obj.type = 'audio';
-        }
+            if (/\.jpg|\.png|\.jpeg|\.webp|\.svg|\.gif/.test(url)) {
+              obj.src = url;
+              obj.type = 'image';
+            } else if (/\.mp4|\.mov/.test(url)) {
+              obj.src = url;
+              obj.type = 'video';
+            } else if (/\.mp3|\.wav/.test(url)) {
+              obj.src = url;
+              obj.type = 'audio';
+            }
 
-        internalFiles.push(obj);
-      })
-    );
-
-    internalFiles = [...internalFiles];
+            return obj;
+          } catch (err) {
+            console.error(err);
+          }
+        })
+      )
+    ).filter(Boolean);
   };
 
   function mousemove(e) {
@@ -261,7 +279,7 @@
 
   $: {
     internalFiles = [];
-    if (value) {
+    if (value && (!Array.isArray(value) || value.length)) {
       loading = true;
       loadFiles(value);
       loading = false;
@@ -285,7 +303,7 @@
   class:fullBorder={hoveringFile}
   on:dragover|preventDefault={() => (hoveringFile = true)}
 >
-{#if loading}
+  {#if loading}
     <div class="loader">
       <div class="spinner"></div>
     </div>
@@ -328,18 +346,51 @@
                 type="button"
                 class="file-remove"
                 on:mousedown|preventDefault={() => removeFile(index)}
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
+              ><path
+                d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+              /></svg
+            >
+          </button>
+          
+          {/if}
+          <div class="file-icon">
+            {#if file.src}
+              {#if file.type === 'image'}
+                <img src={file.src} alt={file.name} />
+              {:else if file.type === 'video'}
+                <!-- svelte-ignore a11y-media-has-caption -->
+                <video controls>
+                  <source src={file.src} />
+                </video>
+              {:else if file.type === 'audio'}
+                <div class="audio-wrapper">
+                  <audio controls>
+                    <source src={file.src} />
+                  </audio>
+                </div>
+              {/if}
+            {:else if file.external}
+              <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 0 640 512"
+                ><path
+                  d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
+                /></svg
+              >
+            {:else}
+              <svg xmlns="http://www.w3.org/2000/svg" height="100px" viewBox="0 0 384 512"
+                ><path
+                  d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"
+                /></svg
               >
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"
                   ><path
                     d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
                   /></svg
                 >
-              </button>
             {/if}
-            <div class="file-icon">
               {#if file.src}
                 {#if file.type === 'image'}
-                  <img src={file.src} alt={file.name} />
                   {#if cropperEnable != cropOptions.disabled}
                     <button
                       type="button"
@@ -386,17 +437,17 @@
               {/if}
             </div>
             <div class="file-name">
-              {file.name}
-            </div>
-            <div class="file-info">
-              <span>
-                {file.size || ''}
-              </span>
-              <span style="color: rgb(26 151 26)">
-                {file.saved ? 'saved' : ''}
-              </span>
-            </div>
+            {file.displayedName}
           </div>
+          <div class="file-info">
+            <span>
+              {file.size || ''}
+            </span>
+            <span style="color: rgb(26 151 26)">
+              {file.saved ? wording.SAVED : ''}
+            </span>
+          </div>
+        </div>
         {:else}
           <Cropper
             maxImgHeight="85%"
@@ -458,7 +509,7 @@
 />
 <input type="text" {id} {name} bind:value={internalValue} hidden />
 
-<style>
+<style lang="postcss">
   .grab {
     cursor: grab;
   }
@@ -654,7 +705,7 @@
       grid-template-columns: repeat(2, minmax(0, 1fr));
     }
   }
-  
+
   @keyframes spinner-bulqg1 {
     0% {
       clip-path: polygon(50% 50%, 0 0, 50% 0%, 50% 0%, 50% 0%, 50% 0%, 50% 0%);
