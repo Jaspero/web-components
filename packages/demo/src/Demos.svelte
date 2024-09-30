5d697071b31bb74416907f87223928a175645708
AnteSuvala <antejunior2003@gmail.com>
2024-09-30 14:43:57 +0200

    demos update

--- packages/demo/src/Demos.svelte
+++ packages/demo/src/Demos.svelte
@@ -11,6 +11,7 @@
   import '../../../dist/quill.wc';
   import '../../../dist/input.wc';
   import '../../../dist/multiselect.wc';
+  import '../../../dist/multiselect.css';
   import '../../../dist/multisearch.wc';
   import '../../../dist/file-upload.wc';
   import '../../../dist/file-list.wc';
