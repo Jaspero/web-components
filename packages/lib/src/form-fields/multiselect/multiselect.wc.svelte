1dd6f1e9feb25679dbd73fa421a30cf91e6a72ab
AnteSuvala <antejunior2003@gmail.com>
2024-10-01 14:00:17 +0200

    fixing multiselect

--- packages/lib/src/form-fields/multiselect/multiselect.wc.svelte
+++ packages/lib/src/form-fields/multiselect/multiselect.wc.svelte
@@ -367,14 +367,14 @@
   <button
     type="button"
     class="jp-multiselect-select"
-    class:jp-multiselect-toggled={open}
+    class:toggled={open}
     bind:this={bindingElement}
     {disabled}
     on:click|preventDefault={toggleMenu}
     on:keydown={handleKeydown}
   >
     {#if label && labelType == 'inside'}
-      <span class="jp-multiselect-select-label" class:jp-multiselect-select-label-move={internalValue || open}>
+      <span class="jp-multiselect-select-label" class:move={internalValue || open}>
         {@html label}
       </span>
     {/if}
@@ -390,7 +390,7 @@
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 320 512"
       class="jp-multiselect-select-arrow"
-      class:jp-multiselect-rotate={open}
+      class:rotate={open}
     >
       <path
         d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
@@ -418,7 +418,7 @@
           <button
             type="button"
             class="jp-multiselect-menu-button"
-            class:jp-multiselect-selected={option.selected}
+            class:selected={option.selected}
             bind:this={optionElements[index]}
             disabled={option.disabled}
             on:click|preventDefault={() => {
