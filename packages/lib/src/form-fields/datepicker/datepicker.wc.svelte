193047bce65828439711c4063ca2a4c90b95a6ea
nikolaloncar02 <nidola07@gmail.com>
2024-10-09 13:55:40 +0200

    Pull SVG icons in to their own files

--- packages/lib/src/form-fields/datepicker/datepicker.wc.svelte
+++ packages/lib/src/form-fields/datepicker/datepicker.wc.svelte
@@ -16,6 +16,11 @@
 />
 
 <script lang="ts">
+  import calendarIcon from '../../icons/calendar.svg?raw';
+  import dropdownArrowExpandedIcon from '../../icons/dropdown-arrow-expanded.svg?raw';
+  import upArrowIcon from '../../icons/up-arrow.svg?raw';
+  import leftArrowIcon from '../../icons/left-arrow.svg?raw';
+  import rightArrowIcon from '../../icons/right-arrow.svg?raw';
   import { clickOutside } from '../../click-outside';
   import { createEventDispatcher } from 'svelte';
   import { formatDisplayDate, formatReturnDate } from '../../utils/dateFormatter';
@@ -387,11 +392,7 @@
     </p>
 
     <span class="field-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <path
-          d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192H400V448c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192z"
-        />
-      </svg>
+      {@html calendarIcon}
     </span>
   </button>
 
@@ -411,11 +412,7 @@
             on:click|preventDefault={() => (yearSelector = true)}
           >
             <p>{monthMap[pickerMonth]}, {pickerYear}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <path
-                d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
-              />
-            </svg>
+            {@html dropdownArrowExpandedIcon}
           </button>
           <div class="menu-nav-buttons">
             <button
@@ -423,22 +420,14 @@
               on:click|preventDefault={() => (pickerMonth = pickerMonth - 1)}
               disabled={internalMinMonthCheck}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                <path
-                  d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-                />
-              </svg>
+              {@html leftArrowIcon}
             </button>
             <button
               type="button"
               on:click|preventDefault={() => (pickerMonth = pickerMonth + 1)}
               disabled={internalMaxMonthCheck}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                <path
-                  d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-                />
-              </svg>
+              {@html rightArrowIcon}
             </button>
           </div>
         </div>
@@ -489,11 +478,7 @@
                     pickerYearRows[pickerYearRows.length - 1].length - 1
                   ]}
                 </p>
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                  <path
-                    d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
-                  />
-                </svg>
+                {@html upArrowIcon}
               </button>
               <div class="menu-year-nav-buttons">
                 <button
@@ -501,22 +486,14 @@
                   on:click|preventDefault={() => yearPickerIndex--}
                   disabled={internalMinYearPageCheck}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                    <path
-                      d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-                    />
-                  </svg>
+                  {@html leftArrowIcon}
                 </button>
                 <button
                   type="button"
                   on:click|preventDefault={() => yearPickerIndex++}
                   disabled={internalMaxYearPageCheck}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                    <path
-                      d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-                    />
-                  </svg>
+                  {@html rightArrowIcon}
                 </button>
               </div>
             </div>
@@ -549,11 +526,7 @@
                 }}
               >
                 <p>{pickerYear}</p>
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                  <path
-                    d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
-                  />
-                </svg>
+                {@html upArrowIcon}
               </button>
               <div class="menu-month-nav-buttons">
                 <button
@@ -561,22 +534,14 @@
                   on:click|preventDefault={() => (pickerYear = pickerYear - 1)}
                   disabled={internalMinYearCheck}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                    <path
-                      d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-                    />
-                  </svg>
+                  {@html leftArrowIcon}
                 </button>
                 <button
                   type="button"
                   on:click|preventDefault={() => (pickerYear = pickerYear + 1)}
                   disabled={internalMaxYearCheck}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
-                    <path
-                      d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-                    />
-                  </svg>
+                  {@html rightArrowIcon}
                 </button>
               </div>
             </div>
--- packages/lib/src/form-fields/place-input.wc.svelte
+++ packages/lib/src/form-fields/place-input.wc.svelte
@@ -20,7 +20,7 @@
   import { onMount, createEventDispatcher } from 'svelte';
   import { clickOutside } from '../click-outside';
   import { loadScript } from '../utils/scriptLoader';
-
+  import locationIcon from '../../icons/location.svg?raw';
   export let apiKey: string;
   export let value = '';
   export let attachedInternals: ElementInternals;
@@ -270,18 +270,7 @@
           on:blur={() => (inputFocused = false)}
         />
         <button class="button" class:active={openMap} on:click|preventDefault={toggleMap}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            class="bi bi-geo-alt-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"
-            />
-          </svg>
+          {@html locationIcon}
         </button>
       </label>
     </div>
