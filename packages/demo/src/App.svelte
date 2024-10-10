10de1dca1a176b96fc415d67591104b9c8467714
AnteSuvala <antejunior2003@gmail.com>
2024-10-10 10:51:04 +0200

    refactor

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
   import '../../lib/src/index.css';
   import AsyncTableDemo from './AsyncTableDemo.svelte';
-  import Demos from './Demos.svelte';
+ //import Demos from './Demos.svelte';
   import FileListDemo from './FileListDemo.svelte';
   import InputDemo from './InputDemo.svelte';
   import MultiSelectDemo from './MultiSelectDemo.svelte';
   import ImageGalleryDemo from './ImageGalleryDemo.svelte';
   import CkEditorDemo from './CkEditorDemo.svelte';
+  import RangeDemo from './RangeDemo.svelte';
 
 </script>
 
-<AsyncTableDemo />
+<!--<AsyncTableDemo /> -->
 <!-- <MultiSelectDemo /> -->
 <!-- <FileListDemo /> -->
-<!-- <Demos /> -->
+ <!--<Demos />-->
 <!-- <InputDemo /> -->
 <!-- <AsyncTableDemo /> -->
 <!-- <ImageGalleryDemo /> -->
  <!-- <CkEditorDemo /> -->
+
+ <RangeDemo />
---
+++ packages/demo/src/RangeDemo.svelte
@@ -0,0 +1,6 @@
+<script>
+  import '../../../dist/range.wc';
+  import '../../../dist/range.css';
+</script>
+
+<jp-range></jp-range>
