ab71e98e20bd6eba4e723084547c65eca0cf426b
AnteSuvala <antejunior2003@gmail.com>
2024-10-10 14:18:06 +0200

    feat: added toggle demo

---
+++ packages/demo/src/ToggleDemo.svelte
@@ -0,0 +1,8 @@
+<script>
+    import '../../../dist/toggle.wc';
+    import '../../../dist/toggle.css';
+</script>
+
+<jp-toggle label="toggle small" required=true></jp-toggle>
+<jp-toggle label="toggle" disabled=true></jp-toggle>
+<jp-toggle label="toggle large" required=true size="large"></jp-toggle>
