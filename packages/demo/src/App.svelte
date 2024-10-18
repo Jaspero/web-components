1ec552bc622066458be83b91cee7d6adffeb7a07
AnteSuvala <antejunior2003@gmail.com>
2024-10-18 13:22:31 +0200

    chore: adding demos

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
   import '../../lib/src/index.css';
-  import AsyncTableDemo from './AsyncTableDemo.svelte';
+  import AsyncTableDemo from './OtherDemos/AsyncTableDemo.svelte';
   import Demos from './Demos.svelte';
   import FileListDemo from './FormFieldDemos/FileListDemo.svelte';
   import InputDemo from './FormFieldDemos/InputDemo.svelte';
   import MultiSelectDemo from './FormFieldDemos/MultiSelectDemo.svelte';
-  import ImageGalleryDemo from './ImageGalleryDemo.svelte';
+  import ImageGalleryDemo from './OtherDemos/ImageGalleryDemo.svelte';
   import CkEditorDemo from './FormFieldDemos/CkEditorDemo.svelte';
   import DateRangeDemo from './FormFieldDemos/DateRangeDemo.svelte';
   import RangeDemo from './FormFieldDemos/RangeDemo.svelte';
   import ChipsDemo from './FormFieldDemos/ChipsDemo.svelte';
   import MultiSearchDemo from './FormFieldDemos/MultiSearchDemo.svelte';
   import SelectDemo from './FormFieldDemos/SelectDemo.svelte';
-  import AssetManagerDemo from './AssetManagerDemo.svelte';
+  import AssetManagerDemo from './OtherDemos/AssetManagerDemo.svelte';
   import AutocompleteDemo from './FormFieldDemos/AutocompleteDemo.svelte';
   import CheckboxDemo from './FormFieldDemos/CheckboxDemo.svelte';
   import ColorDemo from './FormFieldDemos/ColorDemo.svelte';
@@ -24,27 +24,45 @@
   import Textarea from './FormFieldDemos/TextareaDemo.svelte';
   import TextareaDemo from './FormFieldDemos/TextareaDemo.svelte';
   import ToggleDemo from './FormFieldDemos/ToggleDemo.svelte';
+  import CodeEditorDemo from './EditorsDemos/CodeEditorDemo.svelte';
+  import JsonEditorDemos from './EditorsDemos/JSONEditorDemos.svelte';
+  import AccordionDemo from './OtherDemos/AccordionDemo.svelte';
+  import AlertDemo from './OtherDemos/AlertDemo.svelte';
+  import ButtonsDemo from './OtherDemos/ButtonsDemo.svelte';
+  import CarouselDemo from './OtherDemos/CarouselDemo.svelte';
 </script>
 
-<!--<AutocompleteDemo />-->
-<!--<CheckboxDemo /> -->
-<!--<ChipsDemo /> -->
-<!--<CkEditorDemo /> -->
-<!--<ColorDemo /> -->
-<!--<DateRangeDemo /> -->
-<!--<DatepickerDemo /> -->
-<!--<FileListDemo />-->
-<!-- <FileUploadDemo />-->
-<!--<InputDemo />-->
-<!--<MultiSearchDemo />-->
-<!--<MultiSelectDemo />-->
-<!--<QuillDemo /> -->
-<!--<RadioDemo /> -->
-<!--<RangeDemo />-->
-<!-- <SelectDemo /> -->
-<!-- <SliderDemo /> -->
-<!-- <TextareaDemo /> -->
- <ToggleDemo />
-<!--<AssetManagerDemo />-->
-<!--<AsyncTableDemo />-->
-<!--<ImageGalleryDemo />-->
+<div class="form fields">
+  <!-- <AutocompleteDemo /> -->
+  <!-- <CheckboxDemo /> -->
+  <!-- <ChipsDemo /> -->
+  <!-- <CkEditorDemo /> -->
+  <!-- <ColorDemo /> -->
+  <!-- <DateRangeDemo /> -->
+  <!-- <DatepickerDemo /> -->
+  <!-- <FileListDemo /> -->
+  <!-- <FileUploadDemo /> -->
+  <!-- <InputDemo /> -->
+  <!-- <MultiSearchDemo /> -->
+  <!-- <MultiSelectDemo /> -->  
+  <!-- <QuillDemo /> -->
+  <!-- <RadioDemo /> -->
+  <!-- <RangeDemo /> -->
+  <!-- <SelectDemo /> -->
+   <SliderDemo /> 
+  <!-- <TextareaDemo /> -->
+  <!-- <ToggleDemo /> -->
+</div>
+<div class="editors">
+  <!-- <CodeEditorDemo /> -->
+  <!-- <JsonEditorDemos /> -->
+</div>
+<div class="other editors">
+  <!-- <AccordionDemo/> -->
+  <!-- <AlertDemo /> -->
+  <!-- <ButtonsDemo/> -->
+  <!-- <CarouselDemo />-->
+  <!-- <AssetManagerDemo /> --> 
+  <!-- <AsyncTableDemo /> --> 
+  <!-- <ImageGalleryDemo />-->  
+</div>
---
+++ packages/demo/src/OtherDemos/ImageGalleryDemo.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import '../../../../dist/image-gallery.wc.js';
+    // @ts-ignore
+    const images = [
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' },
+      { src: 'https://picsum.photos/300/300' },
+      { src: 'https://picsum.photos/600/700' },
+      { src: 'https://picsum.photos/500/500' },
+      { src: 'https://picsum.photos/200/300' },
+      { src: 'https://picsum.photos/800/400' }
+    ];
+</script>
+
+<jp-image-gallery {images} />
