39077204096a620e7dc4b63bd3a4a63b52f0d9df
AnteSuvala <antejunior2003@gmail.com>
2024-10-21 11:23:23 +0200

    chore: added Data paginator demo

--- packages/demo/src/OtherDemos/DataPaginatorDemo.svelte
+++ packages/demo/src/OtherDemos/DataPaginatorDemo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import '../../../../dist/data-paginator.wc.js';
-  const pageSizeOptions = [11, 7, 1, 8];
+  const pageSizeOptions = [11, 15, 7, 1, 8];
 </script>
 
 <div>
@@ -9,15 +9,6 @@
 </div>
 
 <div>
-  page index
-  <jp-data-paginator pageIndex="30" />
-</div>
-
-<div>
   page size
-  <jp-data-paginator pageSize="15" />
-</div>
-
-<div>
-  <jp-data-paginator {pageSizeOptions} pageSize="15" />
+  <jp-data-paginator {pageSizeOptions}/>
 </div>
