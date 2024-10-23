4e0fa1c532f76aae122d5370b605343d1c3220e8
AnteSuvala <antejunior2003@gmail.com>
2024-10-23 09:43:55 +0200

    chore: added demos

--- packages/demo/src/OtherDemos/ImageGalleryDemo.svelte
+++ packages/demo/src/OtherDemos/ImageGalleryDemo.svelte
@@ -1,37 +1,57 @@
 <script lang="ts">
   import '../../../../dist/image-gallery.wc.js';
-    // @ts-ignore
-    const images = [
-      { src: 'https://picsum.photos/200/300' },
-      { src: 'https://picsum.photos/800/400' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/600/700' },
-      { src: 'https://picsum.photos/500/500' },
-      { src: 'https://picsum.photos/200/300' },
-      { src: 'https://picsum.photos/800/400' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/600/700' },
-      { src: 'https://picsum.photos/500/500' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/200/300' },
-      { src: 'https://picsum.photos/800/400' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/600/700' },
-      { src: 'https://picsum.photos/500/500' },
-      { src: 'https://picsum.photos/200/300' },
-      { src: 'https://picsum.photos/800/400' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/600/700' },
-      { src: 'https://picsum.photos/500/500' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/200/300' },
-      { src: 'https://picsum.photos/800/400' },
-      { src: 'https://picsum.photos/300/300' },
-      { src: 'https://picsum.photos/600/700' },
-      { src: 'https://picsum.photos/500/500' },
-      { src: 'https://picsum.photos/200/300' },
-      { src: 'https://picsum.photos/800/400' }
-    ];
+  const images = [
+    { src: 'https://picsum.photos/200/300' },
+    { src: 'https://picsum.photos/800/400' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/600/700' },
+    { src: 'https://picsum.photos/500/500' },
+    { src: 'https://picsum.photos/200/300' },
+    { src: 'https://picsum.photos/800/400' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/600/700' },
+    { src: 'https://picsum.photos/500/500' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/200/300' },
+    { src: 'https://picsum.photos/800/400' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/600/700' },
+    { src: 'https://picsum.photos/500/500' },
+    { src: 'https://picsum.photos/200/300' },
+    { src: 'https://picsum.photos/800/400' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/600/700' },
+    { src: 'https://picsum.photos/500/500' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/200/300' },
+    { src: 'https://picsum.photos/800/400' },
+    { src: 'https://picsum.photos/300/300' },
+    { src: 'https://picsum.photos/600/700' },
+    { src: 'https://picsum.photos/500/500' },
+    { src: 'https://picsum.photos/200/300' },
+    { src: 'https://picsum.photos/800/400' }
+  ];
 </script>
 
-<jp-image-gallery {images} />
+<div>
+  <jp-image-gallery {images} />
+  default
+</div>
+<div>
+  <jp-image-gallery {images} enablePagination="true" />
+  Pagination
+</div>
+<div>
+  <jp-image-gallery {images} sliderBar="true" />
+  Slider Bar
+</div>
+<div>
+  <jp-image-gallery {images} autoSlide="true" interval="2000" />
+  AutoSlide
+</div>
+
+<style>
+  div {
+    margin-bottom: 400px;
+  }
+</style>
---
+++ packages/demo/src/OtherDemos/MenuDemo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import '../../../../dist/menu.wc.js';
+  function hello() {
+    alert('Hello');
+  }
+</script>
+
+<jp-menu>
+  <button style={"width: 200px;"} on:click={() => alert('first')}> First item </button>
+  <button style={ "width: 200px;" } on:click={() => alert('second')}> Second item </button>
+  <button style={ "width: 200px;" } on:click={() => alert('third')}> Third item </button>
+</jp-menu>
