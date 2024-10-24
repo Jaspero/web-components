7229b49dcef7854f1cd8c6bdd6f0b757994eeaa7
AnteSuvala <antejunior2003@gmail.com>
2024-10-24 14:17:14 +0200

    chore: include demo files in app

--- packages/demo/src/App.svelte
+++ packages/demo/src/App.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
   import '../../lib/src/index.css';
-  import AsyncTableDemo from './OtherDemos/AsyncTableDemo.svelte';
-  import Demos from './Demos.svelte';
-  import FileListDemo from './FormFieldDemos/FileListDemo.svelte';
-  import InputDemo from './FormFieldDemos/InputDemo.svelte';
-  import MultiSelectDemo from './FormFieldDemos/MultiSelectDemo.svelte';
-  import ImageGalleryDemo from './OtherDemos/ImageGalleryDemo.svelte';
-  import CkEditorDemo from './FormFieldDemos/CkEditorDemo.svelte';
-  import DateRangeDemo from './FormFieldDemos/DateRangeDemo.svelte';
-  import RangeDemo from './FormFieldDemos/RangeDemo.svelte';
-  import ChipsDemo from './FormFieldDemos/ChipsDemo.svelte';
-  import MultiSearchDemo from './FormFieldDemos/MultiSearchDemo.svelte';
-  import SelectDemo from './FormFieldDemos/SelectDemo.svelte';
-  import AssetManagerDemo from './OtherDemos/AssetManagerDemo.svelte';
+  //FORM FIELDS
   import AutocompleteDemo from './FormFieldDemos/AutocompleteDemo.svelte';
   import CheckboxDemo from './FormFieldDemos/CheckboxDemo.svelte';
+  import ChipsDemo from './FormFieldDemos/ChipsDemo.svelte';
+  import CkEditorDemo from './FormFieldDemos/CkEditorDemo.svelte';
   import ColorDemo from './FormFieldDemos/ColorDemo.svelte';
+  import DateRangeDemo from './FormFieldDemos/DateRangeDemo.svelte';
   import DatepickerDemo from './FormFieldDemos/DatepickerDemo.svelte';
+  import FileListDemo from './FormFieldDemos/FileListDemo.svelte';
   import FileUploadDemo from './FormFieldDemos/FileUploadDemo.svelte';
+  import InputDemo from './FormFieldDemos/InputDemo.svelte';
+  import MultiSearchDemo from './FormFieldDemos/MultiSearchDemo.svelte';
+  import MultiSelectDemo from './FormFieldDemos/MultiSelectDemo.svelte';
   import QuillDemo from './FormFieldDemos/QuillDemo.svelte';
   import RadioDemo from './FormFieldDemos/RadioDemo.svelte';
+  import RangeDemo from './FormFieldDemos/RangeDemo.svelte';
+  import SelectDemo from './FormFieldDemos/SelectDemo.svelte';
   import SliderDemo from './FormFieldDemos/SliderDemo.svelte';
-  import Textarea from './FormFieldDemos/TextareaDemo.svelte';
   import TextareaDemo from './FormFieldDemos/TextareaDemo.svelte';
   import ToggleDemo from './FormFieldDemos/ToggleDemo.svelte';
+  //EDITORS
   import CodeEditorDemo from './EditorsDemos/CodeEditorDemo.svelte';
   import JsonEditorDemos from './EditorsDemos/JSONEditorDemos.svelte';
+  //OTHERS
   import AccordionDemo from './OtherDemos/AccordionDemo.svelte';
   import AlertDemo from './OtherDemos/AlertDemo.svelte';
+  import AssetManagerDemo from './OtherDemos/AssetManagerDemo.svelte';
+  import AsyncTableDemo from './OtherDemos/AsyncTableDemo.svelte';
   import ButtonsDemo from './OtherDemos/ButtonsDemo.svelte';
   import CarouselDemo from './OtherDemos/CarouselDemo.svelte';
   import ConfirmDemo from './OtherDemos/ConfirmDemo.svelte';
   import DataPaginatorDemo from './OtherDemos/DataPaginatorDemo.svelte';
+  import ImageGalleryDemo from './OtherDemos/ImageGalleryDemo.svelte';
   import LinkDemo from './OtherDemos/LinkDemo.svelte';
   import MenuDemo from './OtherDemos/MenuDemo.svelte';
   import PagePaginatorDemo from './OtherDemos/PagePaginatorDemo.svelte';
@@ -42,6 +43,8 @@
   import TabsDemo from './OtherDemos/TabsDemo.svelte';
   import TooltipDemo from './OtherDemos/TooltipDemo.svelte';
   import TreeDemo from './OtherDemos/TreeDemo.svelte';
+
+  import Demos from './Demos.svelte';
 </script>
 
 <div class="layout-container">
@@ -57,11 +60,11 @@
 -->
   <div class="form-container">
     <div class="form fields">
-       <AutocompleteDemo /> 
-       <CheckboxDemo /> 
-       <ChipsDemo /> 
-       <CkEditorDemo />
-       <ColorDemo />
+      <!-- <AutocompleteDemo /> -->
+      <!-- <CheckboxDemo /> -->
+      <!-- <ChipsDemo /> -->
+      <!-- <CkEditorDemo /> -->
+      <!-- <ColorDemo /> -->
       <!-- <DateRangeDemo /> -->
       <!-- <DatepickerDemo /> -->
       <!-- <FileListDemo /> -->
@@ -74,7 +77,7 @@
       <!-- <RangeDemo /> -->
       <!-- <SelectDemo /> -->
       <!-- <SliderDemo /> -->
-      <!-- <TextareaDemo /> -->
+      <!-- <TextareaDemo /> --> 
       <!-- <ToggleDemo /> -->
     </div>
     <div class="editors">
