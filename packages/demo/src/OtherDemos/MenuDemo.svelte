f2614ee846f6c5107e5f52de0a1d9e6c6ecb830c
AnteSuvala <antejunior2003@gmail.com>
2024-10-29 10:31:30 +0100

    chore: adding jp- prefix

--- packages/demo/src/OtherDemos/MenuDemo.svelte
+++ packages/demo/src/OtherDemos/MenuDemo.svelte
@@ -3,7 +3,22 @@
 </script>
 
 <jp-menu>
-  <button style={"width: 200px;"} on:click={() => alert('first')}> First item </button>
-  <button style={ "width: 200px;" } on:click={() => alert('second')}> Second item </button>
-  <button style={ "width: 200px;" } on:click={() => alert('third')}> Third item </button>
+  <button
+    style={'width: 200px; background-color: transparent; border: 0; cursor: pointer;'}
+    on:click={() => alert('first')}
+  >
+    First item
+  </button>
+  <button
+    style={'width: 200px; background-color: transparent; border: 0; cursor: pointer;'}
+    on:click={() => alert('second')}
+  >
+    Second item
+  </button>
+  <button
+    style={'width: 200px; background-color: transparent; border: 0; cursor: pointer;'}
+    on:click={() => alert('third')}
+  >
+    Third item
+  </button>
 </jp-menu>
--- packages/lib/src/confirm/confirm.wc.svelte
+++ packages/lib/src/confirm/confirm.wc.svelte
@@ -24,39 +24,39 @@
   on:keydown|preventDefault={(e) => e.key === 'Escape' && closable && dispatch('close')}
 />
 
-<div class="overlay">
+<div class="jp-confirm-overlay">
   <div
-    class="dialog"
+    class="jp-confirm-dialog"
     use:clickOutside
     on:click_outside={() => {
       if (closable) dispatch('close');
     }}
   >
     {#if title}
-      <div class="dialog-header">
-        <h1 class="dialog-header-title">
+      <div class="jp-confirm-dialog-header">
+        <h1 class="jp-confirm-dialog-header-title">
           {@html title}
         </h1>
       </div>
     {/if}
 
     {#if message}
-      <p class="dialog-content" class:border-top={title}>
+      <p class="jp-confirm-dialog-content" class:jp-confirm-dialog-content-border-top={title}>
         {@html message}
       </p>
     {/if}
 
-    <div class="dialog-actions">
+    <div class="jp-confirm-dialog-actions">
       <button
         type="button"
-        class="reject-button"
+        class="jp-confirm-reject-button"
         on:click={() => dispatch('confirmation', { confirmed: false })}
       >
         {reject}
       </button>
       <button
         type="button"
-        class="accept-button"
+        class="jp-confirm-accept-button"
         on:click={() => dispatch('confirmation', { confirmed: true })}
       >
         {accept}
@@ -66,90 +66,91 @@
 </div>
 
 <style lang="postcss">
-  .overlay {
-    z-index: 10;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  .jp-confirm {
+    &-overlay {
+      z-index: 10;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
 
-    display: flex;
+      display: flex;
 
-    justify-content: center;
+      justify-content: center;
 
-    align-items: center;
-    background-color: rgba(0, 0, 0, 0.2);
-  }
+      align-items: center;
+      background-color: rgba(0, 0, 0, 0.2);
+    }
 
-  .dialog {
-    background-color: var(--background-primary);
+    &-dialog {
+      background-color: var(--background-primary);
 
-    border-radius: 0.25rem;
-    max-width: 20rem;
-  }
+      border-radius: 0.25rem;
+      max-width: 20rem;
 
-  .dialog-header {
-    padding: 1rem;
-  }
+      &-header {
+        padding: 1rem;
 
-  .dialog-header-title {
-    font-size: 1.5rem;
-  }
+        &-title {
+          font-size: 1.5rem;
+        }
+      }
 
-  .dialog-content {
-    padding: 2rem 1rem;
-  }
+      &-content {
+        padding: 2rem 1rem;
 
-  .dialog-content.border-top {
-    border-top: 1px solid var(--border-primary);
-  }
+        &-border-top {
+          border-top: 1px solid var(--border-primary);
+        }
+      }
+      &-actions {
+        display: flex;
 
-  .dialog-actions {
-    display: flex;
+        justify-content: space-between;
+        border-top: 1px solid var(--border-primary);
+        padding: 1rem;
+      }
+    }
 
-    justify-content: space-between;
-    border-top: 1px solid var(--border-primary);
-    padding: 1rem;
-  }
+    &-reject-button,
+    &-accept-button {
+      position: relative;
 
-  .reject-button,
-  .accept-button {
-    position: relative;
+      display: inline-flex;
 
-    display: inline-flex;
+      align-items: center;
 
-    align-items: center;
+      justify-content: center;
+      min-width: 4rem;
+      padding: 0 1rem;
 
-    justify-content: center;
-    min-width: 4rem;
-    padding: 0 1rem;
+      border-radius: 0.25rem;
+      height: 36px;
+      border: none;
+      outline: none;
+      cursor: pointer;
 
-    border-radius: 0.25rem;
-    height: 36px;
-    border: none;
-    outline: none;
-    cursor: pointer;
+      user-select: none;
+      vertical-align: middle;
+      text-decoration: none;
+      background: none;
+      color: inherit;
+      font-family: inherit;
+      font-size: 0.875rem;
+      line-height: 1.5;
+      text-transform: uppercase;
+      overflow: hidden;
+    }
 
-    user-select: none;
-    vertical-align: middle;
-    text-decoration: none;
-    background: none;
-    color: inherit;
-    font-family: inherit;
-    font-size: 0.875rem;
-    line-height: 1.5;
-    text-transform: uppercase;
-    overflow: hidden;
-  }
+    &-reject-button {
+      color: var(--danger-color);
+      border: 1px solid var(--danger-color);
+    }
 
-  .reject-button {
-    color: var(--danger-color);
-    border: 1px solid var(--danger-color);
-  }
-
-  .accept-button {
-    background-color: var(--primary-color);
-    color: var(--text-on-primary);
+    &-accept-button {
+      background-color: var(--primary-color);
+      color: var(--text-on-primary);
+    }
   }
 </style>
