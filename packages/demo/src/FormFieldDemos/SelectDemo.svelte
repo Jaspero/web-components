cfd40233785601b777356b3408c2c809498e55be
AnteSuvala <antejunior2003@gmail.com>
2024-11-07 13:31:42 +0100

    updated selectDemo

--- packages/demo/src/FormFieldDemos/SelectDemo.svelte
+++ packages/demo/src/FormFieldDemos/SelectDemo.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   import '../../../../dist/select.wc.js';
+  import '../../../../dist/range.wc';
+  import '../../../../dist/range.css';
   let options = [{ value: 'aaa' }, { value: 'bbb' }, { value: 'ccc' }];
 </script>
 
 <form>
   <jp-select {options} label="Select disabled ne required" disabled=true />
-  <jp-select {options} label="Select hidden required" hidden required=true/>
-  <jp-select {options} label="Select display none ne requirted"  style="display: none;"/>
+  <jp-select {options} label="Select hidden required" hidden=true required=true/>
+  <jp-select {options} label="Select display none ne required"  style="display: none;"/>
   <jp-select {options} label="Select disabled display none required" style="display: none;" required=true/>
   <jp-select {options} label="Select og req" required=true/>
-  <jp-select {options} label="Select disabled required" required=true disabled=true />
 
+  <jp-range label="range"></jp-range>
+  <jp-range label="range min" min=20></jp-range>
 
 <button type="submit">submit</button>
 </form>
