0bd40c8d7d211e9f9d706cf2960b4dcd51a29cdb
Dario Zavisic <dzavisic@analyzee.io>
2024-12-05 14:49:24 +0100

    feat(packages): add table chart wc, add demo example for table chart

---
+++ packages/demo/src/OtherDemos/TableChartDemo.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import '../../../../dist/table-chart.wc.js';
+  import { onMount } from 'svelte';
+  import { TABLE_CHART_DATA } from '../lib/consts/table_data.const';
+
+  /**
+   * TYPES
+   */
+  type Config = {
+    dimensions: {
+      capitalize: boolean;
+      active_background: string;
+      border: string;
+      width: string;
+      title: {
+        color: string;
+        font_size: string;
+      }
+    },
+    content: {
+      toolbar: {
+        input: {
+          border: string;
+        },
+        button: {
+          border: string;
+          font_size: string;
+          hover_background_color: string;
+        }
+      },
+      table: {
+        container: {
+          background: string;
+          padding: string;
+          border: string;
+          border_radius: string;
+          border_color: string;
+        },
+        cell: {
+          background: string;
+        },
+        head: {
+          background: string;
+          min_height: string;
+          font_size: string;
+          font_weight: string;
+          padding: string;
+        }
+      }
+      max_height: string;
+      max_width: string;
+      background: string;
+    }
+  };
+
+  /**
+   * VARIABLES
+   */
+  let element: HTMLElement;
+
+  /**
+   * FUNCTIONS
+   */
+  onMount(() => {
+    /**
+     * Table element
+     */
+    const table = document.createElement('jp-table-chart');
+
+    /**
+     * Table data
+     */
+    table.data = TABLE_CHART_DATA;
+
+    /**
+     * Table config
+     */
+    table.config = {
+      dimensions: {
+        capitalize: false,
+        active_background: '#f3f3f3',
+        border: '1px solid #E0E0E0',
+        width: '15vw',
+        title: {
+          color: '#757575',
+          font_size: '0.75rem'
+        }
+      },
+      content: {
+        max_height: '80vh',
+        max_width: '75vw',
+        background: '#75757511'
+      }
+    } as Config;
+
+    element.appendChild(table);
+  });
+</script>
+
+<div bind:this={element}></div>
---
+++ packages/lib/src/table-chart/table-chart.wc.svelte
@@ -0,0 +1,430 @@
+<svelte:options
+  customElement={{
+    tag: 'jp-table-chart',
+    shadow: 'none'
+  }}
+/>
+
+<script lang="ts">
+  /**
+   * TYPES
+   */
+  type Config = {
+    dimensions: {
+      capitalize: boolean;
+      active_background: string;
+      border: string;
+      width: string;
+      title: {
+        color: string;
+        font_size: string;
+      }
+    },
+    content: {
+      toolbar: {
+        input: {
+          border: string;
+        },
+        button: {
+          border: string;
+          font_size: string;
+          hover_background_color: string;
+        }
+      },
+      table: {
+        container: {
+          background: string;
+          padding: string;
+          border: string;
+          border_radius: string;
+          border_color: string;
+        },
+        cell: {
+          background: string;
+        },
+        head: {
+          background: string;
+          min_height: string;
+          font_size: string;
+          font_weight: string;
+          padding: string;
+        }
+      }
+      max_height: string;
+      max_width: string;
+      background: string;
+    }
+  };
+
+  type Dimension = {
+    label: string;
+    value: string;
+    selected: boolean;
+  };
+
+  type Dimensions = Dimension[];
+
+  /**
+   * CONSTS
+   */
+  const DEFAULT_CONFIG: Config = {
+    dimensions: {
+      capitalize: false,
+      active_background: '#f3f3f3',
+      border: '1px solid #E0E0E0',
+      width: '20vw',
+      title: {
+        color: '#757575',
+        font_size: '0.75rem'
+      }
+    },
+    content: {
+      toolbar: {
+        input: {
+          border: '1px solid #f3f3f3'
+        },
+        button: {
+          border: '1px solid #f3f3f3',
+          font_size: '0.875rem',
+          hover_background_color: '#e8e8e8'
+        }
+      },
+      table: {
+        head: {
+          background: '#e3e3e3',
+          min_height: '28px',
+          font_size: '0.75rem',
+          font_weight: 'bold',
+          padding: '0.5rem'
+        },
+        cell: {
+          background: '#f3f3f3'
+        },
+        container: {
+          background: 'blue',
+          padding: '0.5rem',
+          border: '1px solid red',
+          border_radius: '0.25rem',
+          border_color: '#ccc'
+        }
+      },
+      max_height: '80vh',
+      max_width: '75vw',
+      background: '#75757511'
+    }
+  };
+
+  /**
+   * VARIABLES
+   */
+  export let config: Config;
+  export let data: any[];
+
+  let dimensions: Dimensions;
+  let selected_dimensions: Dimensions;
+  let processed_data: { [key: string]: any }[];
+  let sort_order: { [key: string]: 'asc' | 'desc' } = {};
+  let search_value: string = '';
+
+  /**
+   * FUNCTIONS
+   */
+  function select_dimension(dimension: string) {
+    const index = dimensions.findIndex(curr => curr.value === dimension);
+
+    dimensions[index].selected = !dimensions[index].selected;
+
+    const any_selected = dimensions.reduce((curr, acc) => {
+      return acc.selected ? curr + 1 : curr;
+    }, 0);
+
+    selected_dimensions = [];
+
+    if (!any_selected) {
+      return;
+    }
+
+    for (const dimension of dimensions) {
+      if (dimension.selected) {
+        selected_dimensions.push(dimension);
+      }
+    }
+
+    selected_dimensions.push({
+      label: 'Value',
+      value: 'value'
+    });
+  }
+
+  function getNestedValue(obj: any, path: string): any {
+    return path.split('.').reduce((acc, key) => acc && acc[key], obj);
+  }
+
+  function process_data() {
+    const grouped_data: { [key: string]: any } = {};
+
+    const keys = selected_dimensions
+      .map(dimension => dimension.value)
+      .filter(dimension => dimension !== 'value');
+
+    for (const item of data) {
+      const key = keys.map(field => getNestedValue(item, field) || '').join('_');
+
+      if (!grouped_data[key]) {
+        grouped_data[key] = { value: 0 };
+
+        for (const field of keys) {
+          grouped_data[key][field] = getNestedValue(item, field) || '';
+        }
+      }
+
+      grouped_data[key].value += 1;
+    }
+
+    processed_data = Object.values(grouped_data);
+
+    if (Object.keys(sort_order)?.length) {
+      sort_data(Object.keys(sort_order)[0], false);
+    }
+  }
+
+  function sort_data(dimension: string, reverse = true) {
+    if (reverse) {
+      sort_order[dimension] = sort_order[dimension] === 'asc' ? 'desc' : 'asc';
+    }
+
+    for (const key in sort_order) {
+      if (key !== dimension) {
+        delete sort_order[key];
+      }
+    }
+
+    processed_data = processed_data.sort((a, b) => {
+      if (sort_order[dimension] === 'asc') {
+        return a[dimension] > b[dimension] ? 1 : a[dimension] < b[dimension] ? -1 : 0;
+      } else {
+        return a[dimension] < b[dimension] ? 1 : a[dimension] > b[dimension] ? -1 : 0;
+      }
+    });
+  }
+
+  function filter_data() {
+    process_data();
+
+    const filtered_data = processed_data.filter(item => {
+      return Object.values(item).some(value => JSON.stringify(value).toLowerCase().includes(search_value.toLowerCase()));
+    });
+
+    processed_data = (filtered_data?.length && filtered_data) || [];
+  }
+
+  /**
+   * LIFECYCLE
+   */
+  $: if (data?.length) {
+    dimensions = Object.keys(data[0]).map(curr => ({
+      label: config?.dimensions?.capitalize ? curr.charAt(0).toUpperCase() + curr.slice(1) : curr,
+      value: curr,
+      selected: false
+    }));
+  }
+
+  $: if (search_value?.length) {
+    filter_data();
+  }
+
+  $: if (search_value.length === 0 && selected_dimensions?.length) {
+    process_data();
+  }
+</script>
+
+{#if dimensions}
+  <div class="container">
+    <div class="dimensions-picker"
+         style="--active-dimension-background: {config?.dimensions?.active_background || DEFAULT_CONFIG.dimensions.active_background}; --dimensions-picker-border: {config?.content?.toolbar?.input?.border || DEFAULT_CONFIG.content.toolbar.input.border}; --dimensions-picker-width: {config?.dimensions?.width || DEFAULT_CONFIG.dimensions.width}; --dimensions-picker-title-color: {config?.dimensions?.title?.color || DEFAULT_CONFIG.dimensions.title.color}; --dimensions-picker-title-font-size: {config?.dimensions?.title?.font_size || DEFAULT_CONFIG.dimensions.title.font_size};">
+      <span class="dimensions-picker-title">
+        Dimensions
+      </span>
+
+      {#each dimensions as dimension}
+        <div class="selected-dimension hidden" />
+
+        <div
+          class="cursor-pointer dimension {dimension.selected ? 'selected-dimension' : ''}"
+          on:click={() => select_dimension(dimension.value)}
+        >
+          {dimension.label}
+        </div>
+      {/each}
+    </div>
+
+    <div class="table-wrapper-container">
+      {#if (processed_data?.length || search_value?.length) && selected_dimensions?.length}
+        <div class="filters-row"
+             style="--filters-row-input-border: {config?.content?.toolbar?.input?.border || DEFAULT_CONFIG.content.toolbar.input.border}; --filters-row-button-border: {config?.content?.toolbar?.button?.border || DEFAULT_CONFIG.content.toolbar.button.border}; --filters-row-button-font-size: {config?.content?.toolbar?.button?.font_size || DEFAULT_CONFIG.content.toolbar.button.font_size}; --filters-row-button-hover-background-color: {config?.content?.toolbar?.button?.hover_background_color || DEFAULT_CONFIG.content.toolbar.button.hover_background_color};">
+          <input class="filters-row-input cursor-text" type="text" placeholder="Search" bind:value={search_value}>
+
+          <div class="filters-row-button cursor-pointer">
+            Date
+          </div>
+        </div>
+      {/if}
+
+      <div class="table-wrapper"
+           style="--table-wrapper-max-width: {config?.content?.max_width || DEFAULT_CONFIG.content.max_width}; --table-wrapper-max-height: {config?.content?.max_height || DEFAULT_CONFIG.content.max_height}; --table-wrapper-background: {config?.content?.background || DEFAULT_CONFIG.content.background};">
+        {#if selected_dimensions}
+          <div
+            style="--table-cell-background: {config?.content?.table?.cell?.background || DEFAULT_CONFIG.content.table.cell.background}; --table-container-background: {config?.content?.table?.container?.background || DEFAULT_CONFIG.content.table.container.background}; --table-container-padding: {config?.content?.table?.container?.padding || DEFAULT_CONFIG.content.table.container.padding}; --table-head-row-background: {config?.content?.table?.head?.background || DEFAULT_CONFIG.content.table.head.background}; --table-head-row-min-height: {config?.content?.table?.head?.min_height || DEFAULT_CONFIG.content.table.head.min_height}; --table-head-row-font-size: {config?.content?.table?.head?.font_size || DEFAULT_CONFIG.content.table.head.font_size}; --table-head-row-font-weight: {config?.content?.table?.head?.font_weight || DEFAULT_CONFIG.content.table.head.font_weight}; --table-head-row-padding: {config?.content?.table?.head?.padding || DEFAULT_CONFIG.content.table.head.padding}; --table-border: {config?.content?.table?.container?.border || DEFAULT_CONFIG.content.table.container.border}; --table-border-radius: {config?.content?.table?.container?.border_radius || DEFAULT_CONFIG.content.table.container.border_radius}; --table-border-color: {config?.content?.table?.container?.border_color || DEFAULT_CONFIG.content.table.container.border_color};"
+          >
+            <table>
+              <tr class="table-head-row">
+                {#each selected_dimensions as dimension}
+                  <th on:click={() => sort_data(dimension.value)}>
+                    {dimension.label}
+                    {#if sort_order[dimension.value] === 'asc'}↑{:else if sort_order[dimension.value] === 'desc'}↓{/if}
+                  </th>
+                {/each}
+              </tr>
+
+              {#each processed_data as row}
+                <tr>
+                  {#each selected_dimensions as dimension}
+                    <td class="table-cell">
+                      {#if typeof row[dimension.value] === 'object'}
+                        {#each Object.entries(row[dimension.value]) as [key, value]}
+                          <div>
+                            <b>{key}:</b> {value}
+                          </div>
+                        {/each}
+                      {:else}
+                        {row[dimension.value]}
+                      {/if}
+                    </td>
+                  {/each}
+                </tr>
+              {/each}
+            </table>
+          </div>
+        {/if}
+      </div>
+    </div>
+  </div>
+{:else}
+  <div>
+    No data
+  </div>
+{/if}
+
+<style lang="postcss">
+    .hidden {
+        display: none;
+    }
+
+    .cursor-pointer {
+        cursor: pointer;
+    }
+
+    .cursor-text {
+        cursor: text;
+    }
+
+    .container {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        gap: 0.5rem;
+    }
+
+    .table-cell {
+        padding: 0.5rem 1rem;
+        background: var(--table-cell-background);
+    }
+
+    .table-wrapper-container {
+        position: relative;
+    }
+
+    .filters-row {
+        height: 2.5rem;
+        position: absolute;
+        top: 0;
+        right: 0;
+        background-color: white;
+        z-index: 10;
+        display: flex;
+        gap: 0.5rem;
+        justify-content: end;
+        left: 14rem;
+
+        &-input {
+            border: var(--filters-row-input-border);
+            padding: 0.5rem;
+            border-radius: 0.25rem;
+        }
+
+        &-button {
+            border: var(--filters-row-button-border);
+            font-size: var(--filters-row-button-font-size);
+            padding: 0.5rem;
+            border-radius: 0.25rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        &-button:hover {
+            background-color: var(--filters-row-button-hover-background-color);
+        }
+    }
+
+    .table-wrapper {
+        margin-top: 3rem;
+        max-width: var(--table-wrapper-max-width);
+        max-height: var(--table-wrapper-max-height);
+        overflow: auto;
+        background: var(--table-container-background);
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        flex: 1 1 0;
+    }
+
+    .dimensions-picker {
+        border: var(--dimensions-picker-border);
+        width: var(--dimensions-picker-width);
+        padding: 0.5rem;
+        border-radius: 0.25rem;
+        display: flex;
+        gap: 0.5rem;
+        align-items: start;
+        flex-direction: column;
+
+        &-title {
+            color: var(--dimensions-picker-title-color);
+            font-size: var(--dimensions-picker-title-font-size);
+        }
+    }
+
+    .dimension {
+        padding: 0.25rem;
+        border-radius: 0.25rem;
+    }
+
+    .selected-dimension {
+        background-color: var(--active-dimension-background);
+    }
+
+    .table-head-row {
+        min-height: var(--table-head-row-min-height);
+        background-color: var(--table-head-row-background);
+        position: sticky;
+        top: 0;
+        z-index: 2;
+    }
+
+    .table-head-row th {
+        font-size: var(--table-head-row-font-size);
+        font-weight: var(--table-head-row-font-weight);
+        padding: var(--table-head-row-padding);
+    }
+</style>
