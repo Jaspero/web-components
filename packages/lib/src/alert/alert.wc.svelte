f3649697e0a1930f86ca359260e5f447ea6d12f4
kristian <kristiantarnaj@gmail.com>
2025-04-14 11:43:34 +0200

    feat(components): changed alert component style

--- packages/lib/src/alert/alert.wc.svelte
+++ packages/lib/src/alert/alert.wc.svelte
@@ -6,28 +6,33 @@
 />
 
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher, onMount } from 'svelte';
   import './alert.wc.pcss';
   import clearIcon from '../../../lib/src/icons/clear.svg?raw';
 
   export let title = '';
   export let message = '';
-  export let state = 'error';
+  export let state: 'success' | 'error' | '' = '';
+  export let successColor = '#3a8c3d';
+  export let warningColor = '#f44336';
+  export let basicColor = '#ABABAB';
 
   const dispatch = createEventDispatcher();
+
+  onMount(() => {
+    const timer = setTimeout(() => {
+      dispatch('close');
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  });
 </script>
 
-<div class="jp-alert jp-alert-{state}">
+
+<div class="jp-alert jp-alert-{state}" style="--success-c: {successColor}; --warning-c: {warningColor}; --basic-c: {basicColor}">
   <div class="jp-alert-container">
     <div class="jp-alert-container-header">
       <div class="jp-alert-container-header-inner">
-        <div class="jp-alert-container-header-icon">
-          {#if state === 'error'}
-            ⚠️
-          {:else if state === 'success'}
-            ✅
-          {/if}
-        </div>
         {#if title}
           <h2 class="jp-alert-container-header-title">
             {@html title}
@@ -51,5 +56,6 @@
         {@html message}
       </p>
     {/if}
+    <div class="jp-alert-container-bar"></div>
   </div>
 </div>
